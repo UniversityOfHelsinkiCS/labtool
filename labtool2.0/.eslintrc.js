bd5996c233d8edcc6652a9c7e6338fb76916b5b7
Tuomas Junno <tuomas.junno@cs.helsinki.fi>
2018-01-23 12:39:11 +0200

    säädöt

--- labtool2.0/.eslintrc.js
+++ labtool2.0/.eslintrc.js
@@ -1,3 +1,43 @@
 module.exports = {
-    "extends": "google"
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends":[ 
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+
+    "settings": {
+        "react": {
+          "createClass": "createReactClass",
+          "pragma": "React",  
+          "version": "15.0", 
+          "flowVersion": "0.53" 
+        },
+        "propWrapperFunctions": [ "forbidExtraProps" ] 
+    },
+    "rules": {
+        "no-console": 0,
+        "indent": [
+            2,
+            2
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    }
 };
