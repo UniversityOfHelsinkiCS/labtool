8b4d7491b45f252223f212dafda138f1a12e6946
pumm1 <patsu01@gmail.com>
2018-01-26 11:13:24 +0200

    kirjautumisnäkymän proto

--- labtool2.0/src/App.js
+++ labtool2.0/src/App.js
@@ -5,16 +5,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p> Hello world </p>
-        <p> Hello from Inka </p>
-        <p> Hello from Topi </p>
-        <p>Hello from Saku</p>
-        <p> Hello from Joona </p>
-        <p> Hello from Jone </p>
-        <p> Hello from Tuomas </p>
-        <p> Hello from Katri </p>
-        <p> Hello from Pinni </p>
-        <p> Hello from Raul </p>
+      <p>Kirjautumissivu</p>
+
+      <form>
+        <div>
+        <label>
+          Käyttäjätunnus:
+        <input type="text" name="name" />
+        </label>
+        </div>
+        <div>
+          <label>
+            Salasana:
+            <input type="password" name="password"/>  
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
+
+      </form>
       </div>
     );
   }
