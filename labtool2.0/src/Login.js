58285f8255b2d8346e7e5bc012244047aefc4af0
tjunno <tuomas.junno@gmail.com>
2018-01-29 15:36:44 +0200

    eslint errorit

--- labtool2.0/src/Login.js
+++ labtool2.0/src/Login.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 const Login = ({ postLogin, handlePasswordChange, handleUsernameChange, username, password }) => {
-    return (
-      <div className="Login">
-        <p>Kirjautumissivu</p>
-        <form onSubmit={postLogin}>
-          <label>
+  return (
+    <div className="Login">
+      <p>Kirjautumissivu</p>
+      <form onSubmit={postLogin}>
+        <label>
             Käyttäjätunnus: <br />
-            <input value={username} type="text" className="form-control" name="name" onChange={handleUsernameChange} />
-          </label>
-          <label> <br />
+          <input value={username} type="text" className="form-control" name="name" onChange={handleUsernameChange} />
+        </label>
+        <label> <br />
             Salasana: <br />
-            <input value={password} type="password" className="form-control" name="password" onChange={handlePasswordChange} />
-          </label> <br />
-          <button type="submit">Kirjaudu</button>
-        </form>
-      </div>
-    );
-  }
+          <input value={password} type="password" className="form-control" name="password" onChange={handlePasswordChange} />
+        </label> <br />
+        <button type="submit">Kirjaudu</button>
+      </form>
+    </div>
+  );
+};
 
 
 export default Login;
