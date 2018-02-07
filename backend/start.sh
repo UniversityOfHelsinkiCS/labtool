5da51d723c22298b3493e865d323a8040996382b
Raul Becker <raul.becker@iki.fi>
2018-02-07 22:52:02 +0200

    changed: db configuration to actual for environments test and production. Changed also exposed port for backend and start.sh not dropping db. Instead, trying to createdb and then just migrating.

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NPM=`which npm`
-$NPM dropdb
-$NPM createdb
+# $NPM dropdb # not sure if ok since production server db data should persist.
+$NPM createdb # should fail if db already exists.
 $NPM migrate
 $NPM start
