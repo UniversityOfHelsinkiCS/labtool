7c1ed4488e5fa1a1863ff9eccb5b4354d64580fd
Raul Becker <raul.becker@iki.fi>
2018-02-08 02:46:24 +0200

    build another backend container. edited start.sh..

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NPM=`which npm`
 # $NPM dropdb # not sure if ok since production server db data should persist.
-NODE_ENV='test' $NPM createdb # should fail if db already exists.
-NODE_ENV='test' $NPM migrate  # Also should find out how I get docker env.. ahh lol.. lets build.
-NODE_ENV='test' $NPM start    #
+$NPM run createdb # should fail if db already exists.
+$NPM run migrate  # Also should find out how I get docker env.. ahh lol.. lets build.
+$NPM start    #
