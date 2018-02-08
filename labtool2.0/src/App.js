4ed33f8293f39d4eccb16bee0de7483a1eb0b77e
Raul Becker <raul.becker@iki.fi>
2018-02-08 08:28:51 +0200

    build another test frontend container #x
    ...,,,,......... how about now then...

--- labtool2.0/src/App.js
+++ labtool2.0/src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
 
     event.preventDefault()
 
-    let backend = '/backend/login' || 'https://localhost:3001/login'
+    let backend = '/labtool-backend/login' || 'https://localhost:3001/login'
 
     axios.post(backend, {
       username: this.state.username,
