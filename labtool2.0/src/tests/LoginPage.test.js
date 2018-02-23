c275462ee7ad8c9fca1144a388b9d55206b839cf
Katri <katsu4@hotmail.com>
2018-02-23 15:32:02 +0200

    testejä korjailtu, jatkan vielä login

--- labtool2.0/src/tests/LoginPage.test.js
+++ labtool2.0/src/tests/LoginPage.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
 import Login from '../components/pages/LoginPage'
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount, render , wrapper } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json'
 
 
@@ -24,12 +24,40 @@ describe.only('<Login />', () => {
     })
 
     it('renders a username input', () => {
-      expect(shallow(<Login />).find('#name').length).toEqual(0)
+      expect(shallow(<Login />).find('.form-control1').length).toEqual(1)
     })
 
     it('renders a password input', () => {
-      expect(shallow(<Login />).find('#password').length).toEqual(0)
+      expect(shallow(<Login />).find('.form-control2').length).toEqual(1)
     })
+
   })
-})
+  const onSubmit = jest.fn()
+
+  const wrapper = mount(
+    <Login onSubmit={onSubmit} />
+  )
+
+ 
+it('button can be clicked', () => {
+
+
+  const button = wrapper.find('button')
+
+  expect(button.simulate('submit'))
+}) 
+/* jatkan vielä tän tekemistä 
+it('is able to login'), () => {
+const input = wrapper.find('.form-control1')
+const inputt = wrapper.find('.form-control2')
+
+
+ input.simulate('change', { target: { value: 'ss' } })
+ inputt.simulate('change', { target: { value: 'ss' } })
+ 
+ expect(button.simulate('submit'))
+
+ expect(onSubmit.mock.calls.length).toBe(1)
+} */
+}) 
 
