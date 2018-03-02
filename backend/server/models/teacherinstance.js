7baab62c98895e3e9e18ce62b5c07e8d9f4252aa
NorthernLion <joppetaja@gmail.com>
2018-03-02 13:36:51 +0200

    Bugfix

--- backend/server/models/teacherinstance.js
+++ backend/server/models/teacherinstance.js
@@ -1,20 +1,20 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   var TeacherInstance = sequelize.define('TeacherInstance', {}, {})
-  TeacherInstance.associate = function (models) {
+  TeacherInstance.associate = (models) => {
 
-    TeacherInstance.associate = (models) => {
-      TeacherInstance.belongsTo(models.User, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-      })
 
-      TeacherInstance.belongsTo(models.CourseInstance, {
-        foreignKey: 'courseInstanceId',
-        onDelete: 'CASCADE'
-      })
-    }
-    // associations can be defined here
+    TeacherInstance.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE',
+    })
+
+    TeacherInstance.belongsTo(models.CourseInstance, {
+      foreignKey: 'courseInstanceId',
+      onDelete: 'CASCADE'
+    })
   }
+
   return TeacherInstance
+  // associations can be defined here
 }
