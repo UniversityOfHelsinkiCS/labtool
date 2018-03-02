7a3347cb5f9c3836bca3ef90383b0f6a2826090e
jiial <jonelong96@gmail.com>
2018-03-02 12:24:40 +0200

    changed route and fixed id in controller

--- backend/server/routes/userRouter.js
+++ backend/server/routes/userRouter.js
@@ -3,5 +3,5 @@ const userController = require('../controllers').users
 module.exports = (app) => {
   app.post('/api/users', userController.create)
   app.get('/api/users', userController.list)
-  app.put('/api/users/:id', userController.update)
+  app.put('/api/users/update', userController.update)
 }
