451e6ea17e733f698a621ce3270068c912a31130
tjunno <tuomas.junno@cs.helsinki.fi>
2018-03-12 13:02:41 +0200

    fixing continues

--- backend/server/routes/teacherInstanceRouter.js
+++ backend/server/routes/teacherInstanceRouter.js
@@ -1,7 +1,6 @@
 const teacherInstanceController = require('../controllers').teacherInstances
 
 module.exports = (app) => {
-  app.get('/api/courseinstances', courseInstanceController.list)
   //app.post('/api/teacherinstances', teacherInstanceController.create)
   //app.get('/api/teacherinstances', teacherInstanceController.list)
   //app.get('/api/teacherinstances/:id', teacherInstanceController.retrieve)
--- backend/server/routes/weekRouter.js
+++ backend/server/routes/weekRouter.js
@@ -1,7 +1,6 @@
 const weekController = require('../controllers').week
 
 module.exports = (app) => {
-  app.get('/api/courseinstances', courseInstanceController.list)
   // app.post('/api/weeks', weekController.create)
   // app.get('/api/weeks', weekController.list)
   // app.get('/api/weeks/:id', weekController.retrieve)
