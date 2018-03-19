af4c05a78d99922dd83b633baf95751a82875794
jiial <jonelong96@gmail.com>
2018-03-19 14:44:35 +0200

    Static My Page for Teacher/Student

--- labtool2.0/src/App.js
+++ labtool2.0/src/App.js
@@ -6,15 +6,17 @@ import { Container } from 'semantic-ui-react'
 import TestFile from './components/pages/TestFile.js'
 import CoursePage from './components/pages/CoursePage'
 import Email from './components/pages/Email.js'
+import MyPageStudent from './components/pages/MyPageStudent'
+import MyPageTeacher from './components/pages/MyPageTeacher'
 
 
 const App = () => {
   return (
-     <Container >
-    <div>
-      {/* <Header /> */}
-      <Main />
-     </div>
+    <Container >
+      <div>
+        {/* <Header /> */}
+        <Main />
+      </div>
     </Container>
   )
 }
@@ -27,6 +29,8 @@ const Main = () => {
         <Route path={`${process.env.PUBLIC_URL}/courses`} component={Courses} />
         <Route  path={`${process.env.PUBLIC_URL}/coursePage`} component={CoursePage} />
         <Route  path={`${process.env.PUBLIC_URL}/email`} component={Email} />
+        <Route  path={`${process.env.PUBLIC_URL}/myPageStudent`} component={MyPageStudent} />
+        <Route  path={`${process.env.PUBLIC_URL}/myPageTeacher`} component={MyPageTeacher} />
         {/* <Route path='/schedule' component={Schedule} /> */}
       </Switch>
     </main>
