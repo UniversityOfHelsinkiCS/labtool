78d85fead0cfc1cb99bfb96e11306c6cbf2329f0
tjunno <tuomas.junno@cs.helsinki.fi>
2018-04-06 14:26:15 +0300

    lint

--- backend/server/seeders/20180326074307-demo-studentInstance.js
+++ backend/server/seeders/20180326074307-demo-studentInstance.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -12,6 +12,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('StudentInstances', null, {});
+    return queryInterface.bulkDelete('StudentInstances', null, {})
   }
-};
+}
