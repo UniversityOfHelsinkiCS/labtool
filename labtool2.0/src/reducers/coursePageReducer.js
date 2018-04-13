b8c3064241303e294f15e3ed4d4251ef4d3c38b5
NorthernLion <joppetaja@gmail.com>
2018-04-13 13:45:05 +0300

    Added store suport

--- labtool2.0/src/reducers/coursePageReducer.js
+++ labtool2.0/src/reducers/coursePageReducer.js
@@ -3,7 +3,7 @@ const coursePageReducer = (store = {}, action) => {
     case 'COURSE_PAGE_GET_ALL_SUCCESS':
       return action.response
 /*     case 'COURSE_PAGE_GET_ONE':
-      return store.StudentInstances.weeks.find(n => n.id === action.response.week) ??? */
+      return store.StudentInstances.weeks.find(n => n.id === action.response.week) ??? */ 
     default:
       return store
   }
