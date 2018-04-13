00f1bd1fefa3e3fdcdf1caa988522ee77f545087
pumm1 <patsu01@gmail.com>
2018-04-13 13:56:24 +0300

    some things fixed

--- backend/server/controllers/courseinstances.js
+++ backend/server/controllers/courseinstances.js
@@ -37,7 +37,7 @@ module.exports = {
     console.log('TOKEN VERIFIED: ', token)
     console.log('req.params.UserId: ', parseInt(req.body.userId))
     if (token.verified) {
-      db.sequelize.query(`SELECT * FROM "CourseInstances" AS CI JOIN "TeacherInstances" AS TI ON CI.id = TI.id WHERE TI."userId" = ${token.data.id}`)
+      db.sequelize.query(`SELECT * FROM "CourseInstances" AS CI JOIN "TeacherInstances" AS TI ON CI.id = TI.courseInstanceId WHERE TI."userId" = ${token.data.id}`)
         .then(instance =>
           res.status(200).send(instance[0]))
         .catch(error => res.status(400).send(error))
