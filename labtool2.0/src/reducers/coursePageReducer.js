ef685ec41945ba6b9a92c677be550f6c7ed85e51
NorthernLion <joppetaja@gmail.com>
2018-04-13 11:28:27 +0300

    Planing Reducer

---
+++ labtool2.0/src/reducers/coursePageReducer.js
@@ -0,0 +1,13 @@
+const coursePageReducer = (store = {}, action) => {
+  switch (action.type) {
+    case 'COURSE_PAGE_GET_ALL_SUCCESS':
+      return action.response
+/*     case 'COURSE_PAGE_GET_ONE':
+      return store.StudentInstances.weeks.find(n => n.id === action.response.week) ??? */
+    default:
+      return store
+  }
+}
+
+export default coursePageReducer
+
