bb656c5310c016ab4d80095c1e5733f15c8f62b9
Raul Becker <raul.becker@iki.fi>
2018-04-16 18:12:12 +0300

    asdf.. commit so that I can pull at home environment with that contains valid data.

--- backend/bin/test.sh
+++ backend/bin/test.sh
@@ -2,5 +2,5 @@
 ./node_modules/.bin/sequelize db:drop --env $1;
 ./node_modules/.bin/sequelize db:create --env $1 &&
 ./node_modules/.bin/sequelize db:migrate --env $1 &&
-TOKEN=JustSomethingToSatisfyTests NODE_ENV=test ENV=$1 ./node_modules/.bin/mocha -R spec &&
+NODE_ENV=test ENV=$1 ./node_modules/.bin/mocha -R spec &&
 ./node_modules/.bin/sequelize db:drop --env $1
