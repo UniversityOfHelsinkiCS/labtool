32603de9d9e4b9cbbb359286ac8cd313cf63ce4b
Raul Becker <ruupert@users.noreply.github.com>
2018-04-17 16:59:40 +0300

    Update config.js
    changing this back to how you use this by default.

--- backend/server/config/config.js
+++ backend/server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     'username': process.env.DB_USERNAME,
     'password': process.env.DB_PASSWORD,
     'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
+    'host': '127.0.0.1,
     'dialect': 'postgres',
     'operatorsAliases': 'false'
   },
