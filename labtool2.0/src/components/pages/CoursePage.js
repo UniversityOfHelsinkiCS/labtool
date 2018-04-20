16a6bd3f12899ca690ebf5fa20bdcbb05569631d
Katri <katsu3@hotmail.com>
2018-04-20 11:39:25 +0300

    Viikkomäärä dynaaminen, shows only register button if not registered

--- labtool2.0/src/components/pages/CoursePage.js
+++ labtool2.0/src/components/pages/CoursePage.js
@@ -6,7 +6,6 @@ import { connect } from 'react-redux'
 class CoursePage extends Component {
 
 
-
   componentDidMount() {
     console.log(this.props.selectedInstance.id, 'tässä id')
   }
@@ -18,6 +17,13 @@ class CoursePage extends Component {
         instance = this.props.coursePage.data[0]
       }
     }
+
+    var weeks = [];
+    for (var i = 1; i <= this.props.selectedInstance.weekAmount; i++) {
+      weeks.push(<Table.Row> <Table.Cell>{i}</Table.Cell> <Table.Cell>0</Table.Cell> <Table.Cell>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Table.Cell></Table.Row>
+
+      );
+    }
     return (
 
       //const CoursePage = ({ name, start, end, week_amount, week_max_points, current_week, handleFieldChange }) => {
@@ -25,29 +31,46 @@ class CoursePage extends Component {
         <div class="ui grid">
           <div class="sixteen wide column">
             <h2>{this.props.selectedInstance.name}</h2>
+
             {instance !== [] && instance !== undefined && instance ?
               instance.github && this.props.coursePage.role === "student" ?
                 <div>
                   <h2>{instance.projectName}</h2>
                   <h3>{instance.github}</h3>
+
+                  <div class="sixteen wide column" >
+                    <h3> Comments and feedback </h3>
+                    <Table celled padded unstackable>
+                      <Table.Header>
+                        <Table.Row>
+                          <Table.HeaderCell>Week</Table.HeaderCell>
+                          <Table.HeaderCell>Points</Table.HeaderCell>
+                          <Table.HeaderCell>Comment</Table.HeaderCell>
+                        </Table.Row>
+                      </Table.Header>
+                      <Table.Body>
+                        {weeks}
+                      </Table.Body>
+                    </Table> </div>
                 </div> :
                 <Link to={`/labtool/courseregistration/${this.props.selectedInstance.ohid}`}>  <Button>Register</Button></Link> :
               <Link to={`/labtool/courseregistration/${this.props.selectedInstance.ohid}`}>  <Button>Register</Button></Link>
             }
           </div>
 
-          <Table celled >
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Active: {JSON.stringify(this.props.selectedInstance.active)}</Table.HeaderCell>
-                <Table.HeaderCell>Week amount: {this.props.selectedInstance.weekAmount}</Table.HeaderCell>
-                <Table.HeaderCell>Current week: {this.props.selectedInstance.currentWeek}</Table.HeaderCell>
-                <Table.HeaderCell>Week maxpoints: {this.props.selectedInstance.weekMaxPoints}</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-          </Table>
+
           {this.props.coursePage.role == "teacher" ?
             <div class="sixteen wide column" >
+              <Table celled >
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Active: {JSON.stringify(this.props.selectedInstance.active)}</Table.HeaderCell>
+                    <Table.HeaderCell>Week amount: {this.props.selectedInstance.weekAmount}</Table.HeaderCell>
+                    <Table.HeaderCell>Current week: {this.props.selectedInstance.currentWeek}</Table.HeaderCell>
+                    <Table.HeaderCell>Week maxpoints: {this.props.selectedInstance.weekMaxPoints}</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+              </Table>
               <h3> Students </h3>
               <Table celled unstackable>
                 <Table.Header>
@@ -97,44 +120,8 @@ class CoursePage extends Component {
               </Table>
             </div>
             :
-            <div class="sixteen wide column" >
-              <h3> Comments and feedback </h3>
-              <Table celled padded unstackable>
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell>Week</Table.HeaderCell>
-                    <Table.HeaderCell>Points</Table.HeaderCell>
-                    <Table.HeaderCell>Comment</Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  <Table.Row>
-                    <Table.Cell>1</Table.Cell>
-                    <Table.Cell>5</Table.Cell>
-                    <Table.Cell>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ultrices sapien. Aliquam neque diam, scelerisque nec metus sit amet, euismod interdum orci. Vivamus eu convallis ex. Etiam faucibus varius lorem in egestas. Pellentesque quis elementum magna, quis sagittis ex. Mauris a sem dignissim, fringilla elit ac, iaculis quam. Ut ut lacus sit amet massa blandit tincidunt. Mauris mattis tempor nibh, fermentum interdum massa placerat tempor.</Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>2</Table.Cell>
-                    <Table.Cell>2</Table.Cell>
-                    <Table.Cell>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ultrices sapien. Aliquam neque diam, scelerisque nec metus sit amet, euismod interdum orci. Vivamus eu convallis ex. Etiam faucibus varius lorem in egestas. Pellentesque quis elementum magna, quis sagittis ex. Mauris a sem dignissim, fringilla elit ac, iaculis quam. Ut ut lacus sit amet massa blandit tincidunt. Mauris mattis tempor nibh, fermentum interdum massa placerat tempor.</Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>3</Table.Cell>
-                    <Table.Cell>0</Table.Cell>
-                    <Table.Cell>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ultrices sapien. Aliquam neque diam, scelerisque nec metus sit amet, euismod interdum orci. Vivamus eu convallis ex. Etiam faucibus varius lorem in egestas. Pellentesque quis elementum magna, quis sagittis ex. Mauris a sem dignissim, fringilla elit ac, iaculis quam. Ut ut lacus sit amet massa blandit tincidunt. Mauris mattis tempor nibh, fermentum interdum massa placerat tempor.</Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>4</Table.Cell>
-                    <Table.Cell>4</Table.Cell>
-                    <Table.Cell>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ultrices sapien. Aliquam neque diam, scelerisque nec metus sit amet, euismod interdum orci. Vivamus eu convallis ex. Etiam faucibus varius lorem in egestas. Pellentesque quis elementum magna, quis sagittis ex. Mauris a sem dignissim, fringilla elit ac, iaculis quam. Ut ut lacus sit amet massa blandit tincidunt. Mauris mattis tempor nibh, fermentum interdum massa placerat tempor.</Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>5</Table.Cell>
-                    <Table.Cell>4.5</Table.Cell>
-                    <Table.Cell>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ultrices sapien. Aliquam neque diam, scelerisque nec metus sit amet, euismod interdum orci. Vivamus eu convallis ex. Etiam faucibus varius lorem in egestas. Pellentesque quis elementum magna, quis sagittis ex. Mauris a sem dignissim, fringilla elit ac, iaculis quam. Ut ut lacus sit amet massa blandit tincidunt. Mauris mattis tempor nibh, fermentum interdum massa placerat tempor.</Table.Cell>
-                  </Table.Row>
-                </Table.Body>
-              </Table> </div>
+
+            <div> </div>
           }
         </div >
       </div >
