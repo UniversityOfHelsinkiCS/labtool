fe11b924bee3c15fc711d6c7f3dbcd200762042c
jiial <jonelong96@gmail.com>
2018-04-20 14:50:59 +0300

    coursepage näyttää taas jotain

--- labtool2.0/src/components/pages/CoursePage.js
+++ labtool2.0/src/components/pages/CoursePage.js
@@ -17,50 +17,25 @@ class CoursePage extends Component {
         instance = this.props.coursePage.data[0]
       }
     }
+    console.log(instance)
 
-    var weeks = [];
-    var weeksTeacher = [];
+    var weeks = []
+    var weeksTeacher = []
     for (var i = 1; i <= this.props.selectedInstance.weekAmount; i++) {
       weeks.push(<Table.Row> <Table.Cell>{i}</Table.Cell> <Table.Cell>0</Table.Cell> <Table.Cell>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Table.Cell></Table.Row>)
       weeksTeacher.push(<Table.HeaderCell>Week {i}</Table.HeaderCell>)
     }
 
     return (
-    <div className="CoursePage" style={{ textAlignVertical: 'center', textAlign: 'center', }}>
-        <div class="ui grid">
-          <div class="sixteen wide column">
-            <h2>{this.props.selectedInstance.name}</h2>
-
-            {instance !== [] && instance !== undefined && instance ?
-              instance.github && this.props.coursePage.role === "teacher" ?
-                <div>
-                  <h2>{instance.projectName}</h2>
-                  <h3><a href={instance.github} >{instance.github}</a></h3>
-
-                  <div class="sixteen wide column" >
-                    <h3> Comments and feedback </h3>
-                    <Table celled padded unstackable>
-                      <Table.Header>
-                        <Table.Row>
-                          <Table.HeaderCell>Week</Table.HeaderCell>
-                          <Table.HeaderCell>Points</Table.HeaderCell>
-                          <Table.HeaderCell>Comment</Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Header>
-                      <Table.Body>
-                        {weeks}
-                      </Table.Body>
-                    </Table> </div>
-                </div> :
-             <p></p> : <p></p>
-            }
-          </div>
+      <div className="CoursePage" style={{ textAlignVertical: 'center', textAlign: 'center', }}>
+        <div className="ui grid">
+          
 
 
-          {this.props.coursePage.role == "teacher" ?
-            <div class="sixteen wide column" >
+          {this.props.coursePage.role == 'teacher' ?
+            <div >
 
-             <Table celled >
+              <Table celled >
                 <Table.Header>
                   <Table.Row>
                     <Table.HeaderCell>Active: {JSON.stringify(this.props.selectedInstance.active)}</Table.HeaderCell>
@@ -121,13 +96,39 @@ class CoursePage extends Component {
               </Table>
             </div>
             :
+            <div className="sixteen wide column">
+            <h2>{this.props.selectedInstance.name}</h2>
 
-            <div> </div>
+            {instance !== [] && instance !== undefined && instance ?
+              instance.github ?
+                <div>
+                  <h2>{instance.projectName}</h2>
+                  <h3><a href={instance.github} >{instance.github}</a></h3>
+
+                  <div className="sixteen wide column" >
+                    <h3> Comments and feedback </h3>
+                    <Table celled padded unstackable>
+                      <Table.Header>
+                        <Table.Row>
+                          <Table.HeaderCell>Week</Table.HeaderCell>
+                          <Table.HeaderCell>Points</Table.HeaderCell>
+                          <Table.HeaderCell>Comment</Table.HeaderCell>
+                        </Table.Row>
+                      </Table.Header>
+                      <Table.Body>
+                        {weeks}
+                      </Table.Body>
+                    </Table> </div>
+                </div> :
+                <div className="sixteen wide column">
+                  <Link to={`/labtool/courseregistration/${this.props.selectedInstance.ohid}`}>  <Button>Register</Button></Link>
+                </div> : <Link to={`/labtool/courseregistration/${this.props.selectedInstance.ohid}`}>  <Button>Register</Button></Link>
+            }
+          </div>
           }
-        </div >
-      </div >
-    )
-  }
+        </div>
+      </div>
+    )} 
 }
 
 const mapStateToProps = (state) => {
