9a49519db4348157ebe88f125b25d210eecdfddb
Raul Becker <raul.becker@iki.fi>
2018-04-27 13:48:38 +0300

    restore default...

--- backend/server/config/config.js
+++ backend/server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     'username': process.env.DB_USERNAME,
     'password': process.env.DB_PASSWORD,
     'database': process.env.DB_NAME,
-    'host': '10.0.0.10',
+    'host': '127.0.0.1',
     'dialect': 'postgres',
     'operatorsAliases': 'false'
   },
