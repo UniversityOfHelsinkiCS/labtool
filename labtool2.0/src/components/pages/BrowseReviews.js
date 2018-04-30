d1331866c99bdf670b22e35134206bd008584fde
NorthernLion <joppetaja@gmail.com>
2018-04-30 13:59:40 +0300

    It works but it is ugly

--- labtool2.0/src/components/pages/BrowseReviews.js
+++ labtool2.0/src/components/pages/BrowseReviews.js
@@ -15,39 +15,66 @@ class BrowseReviews extends Component {
   }
 
 
-  render() {
-    const headers = []
-    const createHeaders = () => {
-      const studentData = this.props.courseData.data.find(student => student.id === this.props.studentInstance)
-      for (var i = 0; i < this.props.selectedInstance.weekAmount; i++) {
-        headers.push(
-          <Accordion fluid styled>
-            <Accordion.Title active={activeIndex === i} index={i} onClick={this.handleClick}>
-              <Icon name='dropdown' /> Week {i + 1} </Accordion.Title>
-            <Accordion.Content active={activeIndex === i}>
-              <p>
-                <h4> Opiskelijan nimi </h4>
-                <h4> Pisteet </h4>
-                <h4> Kommentit </h4>
-                <Link to={`/labtool/reviewstudent/`}>
-                  <Button circular color="orange" size="tiny" icon="edit black large" />
-                </Link>
 
-              </p>
-            </Accordion.Content>
-          </Accordion>)
-      }
+
+  render() {
+    const createHeaders = (studhead, studentInstance) => {
+      let headers = []
+      studhead.data.map(student => {
+        if (student.id == studentInstance) {
+          for (var i = 0; i < this.props.selectedInstance.weekAmount; i++) {
+            const weekPoints = student.weeks.find(week => week.weekNumber == (i + 1))
+            if (weekPoints) {
+              headers.push(
+                <Accordion fluid styled>
+                  <Accordion.Title active={activeIndex === i} index={i} onClick={this.handleClick}>
+                    <Icon name='dropdown' /> Week {i + 1} </Accordion.Title>
+                  <Accordion.Content active={activeIndex === i}>
+                    <p>
+                      <h4> {student.User.firsts} {student.User.lastname} </h4>
+                      <h4> {weekPoints.points} </h4>
+                      <h4> Comments </h4>
+                      <Link to={`/labtool/reviewstudent/`}>
+                        <Button circular color="orange" size="tiny" icon="edit black large" />
+                      </Link>
+                    </p>
+                  </Accordion.Content>
+                </Accordion>
+              )
+            } else {
+              headers.push(
+                <Accordion fluid styled>
+                  <Accordion.Title active={activeIndex === i} index={i} onClick={this.handleClick}>
+                    <Icon name='dropdown' /> Week {i + 1} </Accordion.Title>
+                  <Accordion.Content active={activeIndex === i}>
+                    <p>
+                      <h4> {student.User.firsts} {student.User.lastname} </h4>
+                      <h4> Not Graded </h4>
+                      <h4> No comments </h4>
+                      <Link to={`/labtool/reviewstudent/`}>
+                        <Button circular color="orange" size="tiny" icon="edit black large" />
+                      </Link>
+                    </p>
+                  </Accordion.Content>
+                </Accordion>
+              )
+            }
+          }
+        }
+      })
       return headers
     }
 
     const { activeIndex } = this.state
 
     return (
-
       <div>
-
-
-        <h2> {this.props.selectedInstance.name}</h2>
+        {this.props.courseData.role === 'teacher'
+          ? <div>
+            <h2> {this.props.selectedInstance.name}</h2>
+            {createHeaders(this.props.courseData, this.props.studentInstance)}
+          </div>
+          : <p></p>}
       </div>
     )
   }
@@ -56,8 +83,6 @@ const mapStateToProps = (state, ownProps) => {
   return {
     ownProps,
     user: state.user,
-    studentInstance: state.studentInstance,
-    teacherInstance: state.teacherInstance,
     selectedInstance: state.selectedInstance,
     courseData: state.coursePage
   }
