ed8baddd976b14f5c10cf40a20602a2b017853ac
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-05-04 01:07:21 +0300

    Courses Page view: added ribbon for marking if course is activated

--- labtool2.0/src/components/pages/Courses.js
+++ labtool2.0/src/components/pages/Courses.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, List, Container, Header, Table, Icon } from 'semantic-ui-react'
+import { Button, List, Container, Header, Table, Label } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
@@ -9,35 +9,41 @@ class Courses extends Component {
       <div>
         <Container>
           <Header as="h2">Courses</Header>
-          <Table celled singleLine>
+          <Table singleLine color="yellow">
             <Table.Header>
               <Table.Row>
+                <Table.HeaderCell colSpan="1"> </Table.HeaderCell>
                 <Table.HeaderCell colSpan="1">Course id</Table.HeaderCell>
                 <Table.HeaderCell colSpan="1">Course name</Table.HeaderCell>
                 <Table.HeaderCell colSpan="1">Course start date</Table.HeaderCell>
-                <Table.HeaderCell colSpan="1">Course active</Table.HeaderCell>
-
-                <Table.HeaderCell colSpan="2" />
+                <Table.HeaderCell colSpan="2"> </Table.HeaderCell>
               </Table.Row>
             </Table.Header>
 
             <Table.Body>
               {this.props.courseInstance.map(instance => (
                 <Table.Row key={''}>
-                  <Table.Cell>{instance.ohid} </Table.Cell>
-                  <Table.Cell selectable>  <a href={`/labtool/courses/${instance.ohid}`}>{instance.name}</a>
-                  </Table.Cell>
-                  <Table.Cell>{instance.start.substring(0, 10)} </Table.Cell>
-                  <Table.Cell>{JSON.stringify(instance.active)} </Table.Cell>
-
-                  <Table.Cell textAlign="center">
+                  <Table.Cell>
                     <div>
                       {instance.active === true ? (
-                        <Button circular size="tiny" icon="large black eye icon" as={Link} to={`/labtool/courses/${instance.ohid}`} style={{ backgroundColor: '#21ba45' }} />
-                    ) : (
-                        <Button circular size="tiny" icon="large black eye icon" as={Link} to={`/labtool/courses/${instance.ohid}`} />
-                      )}
+                        <Label ribbon style={{ backgroundColor: '#21ba45' }}>
+                          Active
+                        </Label>
+                      ) : (
+                          ''
+                        )}
                     </div>
+                  </Table.Cell>
+                  <Table.Cell>{instance.ohid} </Table.Cell>
+                  <Table.Cell>
+                    <bold>
+                      <a href={`/labtool/courses/${instance.ohid}`}>{instance.name}</a>
+                    </bold>
+                  </Table.Cell>
+
+                  <Table.Cell>{instance.start.substring(0, 10)} </Table.Cell>
+                  <Table.Cell textAlign="center">
+                    <Button circular size="tiny" icon="large blue eye icon" as={Link} to={`/labtool/courses/${instance.ohid}`} />
                   </Table.Cell>
                 </Table.Row>
               ))}
@@ -46,7 +52,8 @@ class Courses extends Component {
 
           <div className="Instructions">
             <List>
-              <List.Item icon="eye" content="Show course page" />
+              <List.Item icon="blue eye icon" content="Show course page" />
+              <List.Item icon="green square" content="Course is activated" />
             </List>
           </div>
         </Container>
