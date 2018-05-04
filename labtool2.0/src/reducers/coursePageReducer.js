cc42dd2f2eddc37967ca8e2e4b07cd389d0d0c87
Topi Laanti <laantop@gmail.com>
2018-05-04 13:48:59 +0300

    Course page kommentointia

--- labtool2.0/src/reducers/coursePageReducer.js
+++ labtool2.0/src/reducers/coursePageReducer.js
@@ -1,7 +1,7 @@
 /**
  * The reducer for displaying pretty much all that you want to see
  * on the course page.
- * 
+ *  See detailed documentation of the content below.
  * 
  */
 
@@ -15,3 +15,35 @@ const courseInstancereducer = (store = [], action) => {
 }
 
 export default courseInstancereducer
+
+/**
+ 
+{
+  "role": String, tells if the user is a student or a teacher on this course.
+  "data": {
+      "id": Student or teacher instance id in database
+      "github": String, the github address of the user
+      "projectName": String, Projects name
+      "courseInstanceId": Integer, on which course(id) does this instance belong
+      "userId": Integer, on which user (id) does this instance belong
+      "weeks": [
+          {
+              "id": Integer, database week id
+              "points": Integer, points in course
+              "weekNumber": integer, weeks number
+              "feedback": String, the feedback on it.
+              "studentInstanceId": integer, on which studentinstance does it belong.
+              "comments": [
+                  {
+                      "id": Integer, comments database id
+                      "hidden": boolean that tells whether the comment is a note or feedback
+                      "comment": string, the comments message
+                      "weekId": integer, on which week does the commentbelong
+                      "from": String, the user who commented this.
+                  }
+              ]
+          }
+      ]
+  }
+}
+*/
