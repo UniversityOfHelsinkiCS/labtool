b9994617c065d4ecc9a1961000a4b59c12b5f209
Raul Becker <raul.becker@iki.fi>
2018-05-06 00:22:28 +0300

    numbersnumbers

--- backend/server/helpers/course_instance_helper.js
+++ backend/server/helpers/course_instance_helper.js
@@ -29,7 +29,7 @@ function checkWebOodi(req, res, user, resolve) {
   }
   request(options, function(req, res, body) {
     const json = JSON.parse(body)
-    if (json['students'].toString().match(user.studentnumber) !== null) {
+    if (json['students'].toString().match(user.studentNumber) !== null) {
       // stupid javascript.. even regex match is simpler than json array that has or not has a key of whatever.
       console.log('found')
       resolve('found')
