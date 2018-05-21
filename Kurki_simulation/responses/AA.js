1595444e3b030f4514c237816b022227cb76dfc7
Chamion <jemi.salo@gmail.com>
2018-05-21 15:05:39 +0300

    Base-level setup.

---
+++ Kurki_simulation/responses/AA.js
@@ -0,0 +1,9 @@
+export default {
+	"students": [
+		"014822548",
+		"014600326"
+	],
+	"teachers": [
+		"jemisalo"
+	]
+}
---
+++ Kurki_simulation/responses/courses.js
@@ -0,0 +1,14 @@
+export default [
+	{
+		"name": "Aineopintojen harjoitustyö: Tietokantasovellus (periodi IV)",
+		"starts": "2018-03-12 00:00:00 +0300",
+		"ends": "2018-03-12 00:00:00 +0300",
+		"id": "AA"
+	},
+	{
+		"name": "TOinen kurssi",
+		"starts": "2018-03-12 00:00:00 +0300",
+		"ends": "2018-03-12 00:00:00 +0300",
+		"id": "BB"
+	}
+]
