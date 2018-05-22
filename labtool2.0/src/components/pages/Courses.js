6e6efac733ce7d9caf20ab70a8e9eaeb3e1a630c
laimikko1 <mikko.s.laitinen@helsinki.fi>
2018-05-22 15:56:08 +0300

    I've now made functions that add the required fields in reducer and use those fields in componenents to trender the required forms of date and course ID

--- labtool2.0/src/components/pages/Courses.js
+++ labtool2.0/src/components/pages/Courses.js
@@ -44,9 +44,7 @@ class Courses extends Component {
                     </strong>
                   </Table.Cell>
 
-                  <Table.Cell>
-                    {instance.start.substring(8, 10)}.{instance.start.substring(5, 7)}.{instance.start.substring(0, 4)}{' '}
-                  </Table.Cell>
+                  <Table.Cell> {instance.europeanStart} </Table.Cell>
                   <Table.Cell textAlign="center">
                     <Button circular size="tiny" icon={{ name: 'eye', size: 'large', color: 'blue' }} as={Link} to={`/labtool/courses/${instance.ohid}`} />
                   </Table.Cell>
--- labtool2.0/src/reducers/courseInstanceReducer.js
+++ labtool2.0/src/reducers/courseInstanceReducer.js
@@ -13,10 +13,22 @@
   ohid(pin): "TKT20011.2018.K.A.1" -- Opetushallitus id of the course, is often used instead of the database id
  */
 
+// a few helperfunctions to create european form start / end date and to make a prettier course id `
+
+const createEuropeanDate = date => {
+  return `${date.substring(8, 10)}.${date.substring(5, 7)}.${date.substring(0, 4)}`
+}
+
+const createShorterCourseid = ohid => {
+  return `${ohid.substring(0, 8)}`
+}
+
+
 const courseInstancereducer = (store = [], action) => {
   switch (action.type) {
     case 'CI_GET_ALL_SUCCESS':
-      return action.response
+      let x = action.response.map(m => { return { ...m, europeanStart: createEuropeanDate(m.start), europeanEnd: createEuropeanDate(m.end), shorterId: createShorterCourseid(m.ohid) } })
+      return x
     case 'CI_MODIFY_ONE_SUCCESS':
       return store
     default:
