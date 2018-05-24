799b13a9016ef3a339b1d23281242e5ac8942f8c
Chamion <jemi.salo@gmail.com>
2018-05-24 22:21:13 +0300

    Correct filepath again. This time for sure...

--- backend/start.sh
+++ backend/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if [ -e ~/data/scripts_labtool_2018k/setup.sh ]
+if [ -e /data/scripts_labtool_2018k/setup.sh ]
 then
-~/data/scripts_labtool_2018k/setup.sh
+echo "running setup.sh"
+/data/scripts_labtool_2018k/setup.sh
 fi
 NPM=`which npm`
 # $NPM dropdb # not sure if ok since production server db data should persist.
-$NPM run dropdb
 $NPM run createdb # should fail if db already exists.
 $NPM run migrate  # Also should find out how I get docker env.. ahh lol.. lets build.
 $NPM start #
