e9a0ced923f3a0ea375fb98d9b07348a43f691e0
Chamion <jemi.salo@gmail.com>
2018-05-24 15:05:14 +0300

    Just dropping the db.

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-if [ -e data/scripts_labtool_2018k/setup.sh ]
-then
-data/scripts_labtool_2018k/setup.sh
-fi
 NPM=`which npm`
 # $NPM dropdb # not sure if ok since production server db data should persist.
+$NPM run dropdb
 $NPM run createdb # should fail if db already exists.
 $NPM run migrate  # Also should find out how I get docker env.. ahh lol.. lets build.
 $NPM start #
