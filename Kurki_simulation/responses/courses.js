7d2f1809a5316c5fe29d17c6391d582ebed6966d
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-05-24 14:42:59 +0300

    more data and some renames

--- Kurki_simulation/responses/courses.js
+++ Kurki_simulation/responses/courses.js
@@ -1,16 +1,23 @@
 const courses = [
 	{
-		"name": "Aineopintojen harjoitustyö: Tietokantasovellus (periodi IV)",
-		"starts": "2018-03-12 00:00:00 +0300",
-		"ends": "2018-03-12 00:00:00 +0300",
-		"id": "AA"
+		"name": "Vanhin kurssi",
+		"starts": "2018-01-12 00:00:00 +0300",
+		"ends": "2018-02-12 00:00:00 +0300",
+		"id": "KN1111.2018K.3"
 	},
 	{
-		"name": "Toinen kurssi",
+		"name": "Toiseksi vanhin kurssi",
 		"starts": "2018-03-12 00:00:00 +0300",
-		"ends": "2018-03-12 00:00:00 +0300",
-		"id": "BB"
-	}
+		"ends": "2018-04-12 00:00:00 +0300",
+		"id": "KN2222.2018K"
+	},
+	{
+		"name": "Uusin kurssi",
+		"starts": "2018-04-12 00:00:00 +0300",
+		"ends": "2018-05-12 00:00:00 +0300",
+		"id": "KN1111.2018K.4"
+	},
+
 ];
 
 
