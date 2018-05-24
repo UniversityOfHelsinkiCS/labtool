a997dfab38c5ce8c5a4470d4392bb888981ee122
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-05-24 13:19:35 +0300

    Sequelize script and some test data

--- Kurki_simulation/responses/users.js
+++ Kurki_simulation/responses/users.js
@@ -1,60 +1,11 @@
 const users = [
 
     {
-        "username": "paaope",
+        "username": "paaopettaja",
         "student_number": "014822548",
-        "first_names": "Pää Vastuu",
+        "first_names": "Pää",
         "last_name": "Opettaja"
     },
-
-    {
-        "username": "tiraohja1",
-        "student_number": "012345678",
-        "first_names": "Tira Yksi",
-        "last_name": "Ohjaaja"
-    },
-
-    {
-        "username": "tiraohja2",
-        "student_number": "022789345",
-        "first_names": "Tira Kaksi",
-        "last_name": "Ohjaaja"
-    },
-
-    {
-        "username": "otmohja1",
-        "student_number": "013906520",
-        "first_names": "Ohjelmistotekniikan",
-        "last_name": "Ohjaaja"
-    },
-
-    {
-        "username": "tiraopi1",
-        "student_number": "012719822",
-        "first_names": "Tira Eka",
-        "last_name": "Opiskelija"
-    },
-
-    {
-        "username": "tiraopi2",
-        "student_number": "014276571",
-        "first_names": "Tira Toka",
-        "last_name": "Opiskelija"
-    },
-
-    {
-        "username": "otmopi2",
-        "student_number": "013590369",
-        "first_names": "Otm Eka",
-        "last_name": "Opiskelija"
-    },
-
-    {
-        "username": "otmopi2",
-        "student_number": "014271523",
-        "first_names": "Otm Toka",
-        "last_name": "Opiskelija"
-    }
 
 ]
 
--- backend/server/seeders/20180326074254-demo-courseInstance.js
+++ backend/server/seeders/20180326074254-demo-courseInstance.js
@@ -6,8 +6,41 @@ module.exports = {
       'CourseInstances',
       [
         {
-          name: 'hassuKurssi',
-          ohid: 'TKT5555',
+          id: 1,
+          name: 'Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit',
+          start: '2018-03-11T21:00:00.000Z',
+          end: '2018-04-29T21:00:00.000Z',
+          active: false,
+          weekAmount: 7,
+          weekMaxPoints: 3,
+          currentWeek: 1,
+          ohid: 'TIRA',
+          createdAt: '2018-03-26',
+          updatedAt: '2018-03-26'
+        },
+        {
+          id: 2,
+          name: 'Aineopintojen harjoitustyö: Tietokantasovellus',
+          start: '2018-01-16T21:00:00.000Z',
+          end: '2018-03-10T21:00:00.000Z',
+          active: false,
+          weekAmount: 7,
+          weekMaxPoints: 3,
+          currentWeek: 1,
+          ohid: 'TSOHA',
+          createdAt: '2018-03-26',
+          updatedAt: '2018-03-26'
+        },
+        {
+          id: 3,
+          name: 'Ohjelmistotekniikan menetelmät',
+          start: '2018-03-11T21:00:00.000Z',
+          end: '2018-04-29T21:00:00.000Z',
+          active: false,
+          weekAmount: 7,
+          weekMaxPoints: 3,
+          currentWeek: 1,
+          ohid: 'OTM',
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         }
