66420592492a8480d9c372ffea529bc143e9182e
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-05-28 15:22:15 +0300

    Add 10000 to test data ids

--- backend/server/seeders/20180326074307-demo-studentInstances.js
+++ backend/server/seeders/20180326074307-demo-studentInstances.js
@@ -6,38 +6,38 @@ module.exports = {
       'StudentInstances',
       [
         {
-          id: 11,
+          id: 10011,
           github: 'http://github.com/tiralabra1',
           projectName: 'Tiran labraprojekti',
-          userId: 11,
-          courseInstanceId: 11,
+          userId: 10011,
+          courseInstanceId: 10011,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
         {
-          id: 12,
+          id: 10012,
           github: 'http://github.com/tiralabra2',
           projectName: 'Tiran toinen labraprojekti',
-          userId: 12,
-          courseInstanceId: 11,
+          userId: 10012,
+          courseInstanceId: 10011,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
         {
-          id: 21,
+          id: 10021,
           github: 'http://github.com/otmprojekti1',
           projectName: 'OTM projekti',
-          userId: 21,
-          courseInstanceId: 12,
+          userId: 10021,
+          courseInstanceId: 10012,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
         {
-          id: 22,
+          id: 10022,
           github: 'http://github.com/otmprojekti1',
           projectName: 'Toinen OTM projekti',
-          userId: 22,
-          courseInstanceId: 12,
+          userId: 10022,
+          courseInstanceId: 10012,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         }
--- backend/server/seeders/20180524100431-demo-teacherInstances.js
+++ backend/server/seeders/20180524100431-demo-teacherInstances.js
@@ -6,41 +6,41 @@ module.exports = {
       'TeacherInstances',
       [
         {
-          id: 1,
-          userId: 10,
-          courseInstanceId: 11,
+          id: 10001,
+          userId: 10010,
+          courseInstanceId: 10011,
           admin: true,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
         {
-          id: 2,
-          userId: 10,
-          courseInstanceId: 12,
+          id: 10002,
+          userId: 10010,
+          courseInstanceId: 10012,
           admin: true,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
         {
-          id: 3,
-          userId: 10,
-          courseInstanceId: 13,
+          id: 10003,
+          userId: 10010,
+          courseInstanceId: 10013,
           admin: true,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
         {
-          id: 11,
-          userId: 15,
-          courseInstanceId: 11,
+          id: 10011,
+          userId: 10015,
+          courseInstanceId: 10011,
           admin: true,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
         {
-          id: 12,
-          userId: 25,
-          courseInstanceId: 12,
+          id: 10012,
+          userId: 10025,
+          courseInstanceId: 10012,
           admin: true,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
