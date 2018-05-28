a0da47e7e84321b7305227bfc9e91363ea6475b9
Chamion <jemi.salo@gmail.com>
2018-05-28 10:35:25 +0300

    demo setup 2

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -e /data/scripts_labtool_2018k/setup.sh ]
+if [ -e /data/scripts_labtool_2018k/setup.sh ] # will fail if etup.sh is not found setup.sh should only be found on the test server.
 then
 echo "running setup.sh"
 /data/scripts_labtool_2018k/setup.sh
@@ -8,5 +8,5 @@ NPM=`which npm`
 # $NPM dropdb # not sure if ok since production server db data should persist.
 $NPM run createdb # should fail if db already exists.
 $NPM run migrate  # Also should find out how I get docker env.. ahh lol.. lets build.
-$NPM add_test_data
+$NPM run add_test_data
 $NPM start #
