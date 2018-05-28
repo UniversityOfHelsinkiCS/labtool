57edd24cb6fcee1fd6b3541cef20b83a0f72bfd9
Chamion <jemi.salo@gmail.com>
2018-05-28 10:27:37 +0300

    Demo setup

--- backend/start.sh
+++ backend/start.sh
@@ -8,4 +8,5 @@ NPM=`which npm`
 # $NPM dropdb # not sure if ok since production server db data should persist.
 $NPM run createdb # should fail if db already exists.
 $NPM run migrate  # Also should find out how I get docker env.. ahh lol.. lets build.
+$NPM add_test_data
 $NPM start #
