30df7efe8acfa5b86787df08e866b6cd48f4dad2
Chamion <jemi.salo@gmail.com>
2018-05-30 10:41:46 +0300

    Replaced hardcoded add-test-data with production-friendly implementation.

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -e /data/scripts_labtool_2018k/setup.sh ] # will fail if etup.sh is not found setup.sh should only be found on the test server.
+if [ -e /data/scripts_labtool_2018k/setup.sh ] # will fail if setup.sh is not found. setup.sh should only be found on the test server.
 then
 echo "running setup.sh"
 /data/scripts_labtool_2018k/setup.sh
@@ -8,5 +8,9 @@ NPM=`which npm`
 # $NPM dropdb # not sure if ok since production server db data should persist.
 $NPM run createdb # should fail if db already exists.
 $NPM run migrate  # Also should find out how I get docker env.. ahh lol.. lets build.
-$NPM run add_test_data
+if [ -e /data/scripts_labtool_2018k/before_start.sh ] # Same idea as with setup.sh.
+then
+echo "running before_start.sh"
+/data/scripts_labtool_2018k/before_start.sh
+fi
 $NPM start #
