56b5e187d1bb63d0b7658135b6e5c52fc6991689
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-05-31 15:06:19 +0300

    added missing returns, removed unneccessary console.logs and comment

--- backend/server/controllers/assistants.js
+++ backend/server/controllers/assistants.js
@@ -15,56 +15,51 @@ module.exports = {
     await helper.controller_before_auth_check_action(req, res)
 
     try {
-      console.log('req.body: ', req.body, '\n\n')
       const teacherInsId = req.body.teacherInstanceId
       const studentInsId = req.body.studentInstanceId
 
       if (!req.authenticated.success) {
         res.status(400).send('you have to be authenticated to do this')
+        return
       }
-
+      console.log('\n\nAuthentication succeeded')
       const requestMakerId = req.decoded.id
       const requestMakerAsTeacher = await TeacherInstance.findOne({
         where: {
           userId: requestMakerId
         }
       })
-      // check that request maker is a teacher
       if (!requestMakerAsTeacher) {
         res.status(400).send('You have to be a teacher to give assistants to student.')
+        return
       }
-
       const requestMakersCoursesId = requestMakerAsTeacher.courseInstanceId
       const givenTeachersTeacherInstance = await TeacherInstance.findOne({
         where: {
           id: teacherInsId
         }
       })
-      //check that there is a teacher with the given id
       if (!givenTeachersTeacherInstance) {
         res.status(404).send('There is no teacher with the given teacherInstanceId')
+        return
       }
-
       const teachersCourseId = givenTeachersTeacherInstance.courseInstanceId
-      // check that request maker is a teacher on the same course as the given teacher
       if (teachersCourseId !== requestMakersCoursesId) {
         res.status(400).send('You have to be an assistant or teacher in the same course as the teacher you are adding.')
+        return
       }
       const studentInstance = await StudentInstance.findOne({
         where: {
           id: studentInsId
         }
       })
-      // check that there is a student with the given id
       if (!studentInstance) {
         res.status(404).send('Specified student instance could not be found.')
+        return
       }
-
-      console.log('\n\nFound the student instance')
-      // check that the given teacher's course matches given student's course
       if (studentInstance.courseInstanceId === teachersCourseId) {
-        console.log('\n\nCourses do not match')
         res.states(400).send('The teacher is not from the same course as this student.')
+        return
       }
       console.log('\n\nCourses match')
       studentInstance.updateAttributes({
@@ -72,7 +67,7 @@ module.exports = {
       })
       res.status(200).send('assistanceInstance created')
     } catch (e) {
-      console.log('\n\nassistantInstance creation failed\n\n')
+      console.log('\n\nAssistantInstance creation failed.\n', e)
     }
   },
 
