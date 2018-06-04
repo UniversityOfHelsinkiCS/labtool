229c862f2b763cc3d1f6ec188fc46ed05ce5580e
Chamion <jemi.salo@gmail.com>
2018-06-04 16:25:06 +0300

    Create model and migration for CodeReview.

---
+++ backend/server/models/codereview.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const CodeReview = sequelize.define(
+    'CodeReview',
+    {
+      points: DataTypes.DOUBLE,
+      reviewNumber: DataTypes.INTEGER
+    },
+    {}
+  )
+  CodeReview.associate = models => {
+    CodeReview.belongsTo(models.StudentInstance, {
+      foreignKey: 'studentInstanceId',
+      onDelete: 'CASCADE'
+    })
+    CodeReview.belongsTo(models.StudentInstance, {
+      foreignKey: 'toReview',
+      onDelete: 'CASCADE'
+    })
+  }
+  return CodeReview
+}
