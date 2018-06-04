cb03ca8bd3fe20db73681a8c4cbf4af821cc3430
Chamion <jemi.salo@gmail.com>
2018-06-05 00:32:34 +0300

    Change /api/codereviews/bulkinsert from post to put.

--- backend/server/routes/codeReviewRoutes.js
+++ backend/server/routes/codeReviewRoutes.js
@@ -1,5 +1,12 @@
 const codeReviewController = require('../controllers').codeReviews
 
 module.exports = app => {
-  app.post('/api/codereviews/bulkinsert', codeReviewController.bulkInsert)
+  /**
+   * Expects the following fields in the body:
+   * reviewNumber: reviewNumber to insert
+   * codeReviews: Array of objects with the following fields:
+   *   reviewer: will become the studentInstanceId to insert
+   *   toReview: will become the toReview to insert
+   */
+  app.put('/api/codereviews/bulkinsert', codeReviewController.bulkInsert)
 }
