e8de5698dc324d926034fad59f7ed8c62c3406ce
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-06-05 12:46:55 +0300

    Fix react warning for add assistant button

--- labtool2.0/src/components/pages/ModifyCourseInstancePage.js
+++ labtool2.0/src/components/pages/ModifyCourseInstancePage.js
@@ -121,7 +121,7 @@ export class ModifyCourseInstancePage extends Component {
         </Grid>
 
         <Link to={`/labtool/ModifyCourseInstanceStaff/${this.props.selectedInstance.ohid}`}>
-          <Button floated="center">Add assistant teachers</Button>
+          <Button block="true">Add assistant teachers</Button>
         </Link>
       </div>
     )
