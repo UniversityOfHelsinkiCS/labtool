f3dd408f72076655f423472d0855bea57ee4e8da
ootee <olli.tanttu@helsinki.fi>
2018-06-05 13:44:26 +0300

    Added superopiskelija to the review ring

--- backend/server/seeders/20180605075616-demo-codereviews.js
+++ backend/server/seeders/20180605075616-demo-codereviews.js
@@ -21,10 +21,19 @@ module.exports = {
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26',
           studentInstanceId: 10012,
-          toReview: 10011
+          toReview: 10031
         },
         {
           id: 10003,
+          points: null,
+          reviewNumber: 1,
+          createdAt: '2018-03-26',
+          updatedAt: '2018-03-26',
+          studentInstanceId: 10031,
+          toReview: 10011
+        },
+        {
+          id: 10004,
           points: null,
           reviewNumber: 1,
           createdAt: '2018-03-26',
@@ -33,16 +42,25 @@ module.exports = {
           toReview: 10022
         },
         {
-          id: 10004,
+          id: 10005,
           points: null,
           reviewNumber: 1,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26',
           studentInstanceId: 10022,
+          toReview: 10032
+        },
+        {
+          id: 10006,
+          points: null,
+          reviewNumber: 1,
+          createdAt: '2018-03-26',
+          updatedAt: '2018-03-26',
+          studentInstanceId: 10032,
           toReview: 10021
         },
         {
-          id: 10005,
+          id: 10007,
           points: null,
           reviewNumber: 2,
           createdAt: '2018-03-26',
@@ -51,13 +69,22 @@ module.exports = {
           toReview: 10031
         },
         {
-          id: 10006,
+          id: 10008,
           points: null,
           reviewNumber: 2,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26',
           studentInstanceId: 10031,
-          toReview: 10031
+          toReview: 10012
+        },
+        {
+          id: 10009,
+          points: null,
+          reviewNumber: 2,
+          createdAt: '2018-03-26',
+          updatedAt: '2018-03-26',
+          studentInstanceId: 10012,
+          toReview: 10011
         }
       ],
       {}
