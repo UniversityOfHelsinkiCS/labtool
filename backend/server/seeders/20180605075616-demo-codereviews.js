7b0fcf58ed29c4745fca62c09ae295943560e63b
ootee <olli.tanttu@helsinki.fi>
2018-06-05 13:45:56 +0300

    Remove comment waste

--- backend/server/seeders/20180605075616-demo-codereviews.js
+++ backend/server/seeders/20180605075616-demo-codereviews.js
@@ -89,27 +89,9 @@ module.exports = {
       ],
       {}
     )
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('CodeReviews', null, {})
-
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
   }
 }
