77052e922a1c3c41d3e70c3b0ac81a2dabee8ae9
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-06-05 11:11:53 +0300

    Change admin status for assistant teachers to false

--- backend/server/seeders/20180326074307-demo-teacherInstances.js
+++ backend/server/seeders/20180326074307-demo-teacherInstances.js
@@ -25,7 +25,7 @@ module.exports = {
           id: 10003,
           userId: 10010,
           courseInstanceId: 10013,
-          admin: true,
+          admin: false,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
@@ -33,7 +33,7 @@ module.exports = {
           id: 10011,
           userId: 10015,
           courseInstanceId: 10011,
-          admin: true,
+          admin: false,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         },
@@ -41,7 +41,7 @@ module.exports = {
           id: 10012,
           userId: 10025,
           courseInstanceId: 10012,
-          admin: true,
+          admin: false,
           createdAt: '2018-03-26',
           updatedAt: '2018-03-26'
         }
