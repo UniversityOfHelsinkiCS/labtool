fdc3dab7960f9a1f5a1a3057680cbf2ca958b8ef
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-06-07 10:33:53 +0300

    Modify incorrect float value for buttons

--- labtool2.0/src/components/pages/ModifyCourseInstancePage.js
+++ labtool2.0/src/components/pages/ModifyCourseInstancePage.js
@@ -121,14 +121,10 @@ export class ModifyCourseInstancePage extends Component {
         </Grid>
 
         <Link to={`/labtool/ModifyCourseInstanceStaff/${this.props.selectedInstance.ohid}`}>
-          <Button style={{ marginTop: '10px' }} floated="left">
-            Add assistant teachers
-          </Button>
+          <Button style={{ marginTop: '10px' }}>Add assistant teachers</Button>
         </Link>
         <Link to={`/labtool/ModifyCourseInstanceCodeReviews/${this.props.selectedInstance.ohid}`}>
-          <Button style={{ marginTop: '10px' }} floated="right">
-            Add or modify codereviews
-          </Button>
+          <Button style={{ marginTop: '10px' }}>Add or modify codereviews</Button>
         </Link>
       </div>
     )
