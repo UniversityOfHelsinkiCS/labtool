d588c2b311b5ea694c187177d2a1de5327e0a5b3
Mika Äärilä <maarila@gmail.com>
2018-06-13 11:49:42 +0300

    Add seeders to tags and studenttags

---
+++ backend/server/seeders/20180613072509-demo-tags.js
@@ -0,0 +1,58 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Tags',
+      [
+        {
+          id: 20001,
+          name: 'Javascript',
+          color: 'red',
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 20002,
+          name: 'HTML',
+          color: 'yellow',
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 20003,
+          name: 'game',
+          color: 'black',
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 20004,
+          name: 'React',
+          color: 'green',
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 20005,
+          name: 'Node.js',
+          color: 'blue',
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 20006,
+          name: 'Java',
+          color: 'orange',
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        }
+      ],
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Tags', null, {})
+  }
+}
---
+++ backend/server/seeders/20180613073528-demo-studenttags.js
@@ -0,0 +1,58 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'StudentTags',
+      [
+        {
+          id: 30001,
+          studentInstanceId: 10011,
+          tagId: 20001,
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 30002,
+          studentInstanceId: 10011,
+          tagId: 20002,
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 30003,
+          studentInstanceId: 10011,
+          tagId: 20003,
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 30004,
+          studentInstanceId: 10012,
+          tagId: 20006,
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 30005,
+          studentInstanceId: 10012,
+          tagId: 20005,
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        },
+        {
+          id: 30006,
+          studentInstanceId: 10021,
+          tagId: 20004,
+          createdAt: '2018-06-13',
+          updatedAt: '2018-06-13'
+        }
+      ],
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('StudentTags', null, {})
+  }
+}
