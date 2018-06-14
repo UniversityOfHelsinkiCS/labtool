aff217ecd55587b20238896902a95a0bf6d124b0
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-06-14 15:07:04 +0300

    Remove tags with duplicate id from store

--- labtool2.0/src/reducers/tagReducer.js
+++ labtool2.0/src/reducers/tagReducer.js
@@ -3,7 +3,7 @@ const INITIAL_STATE = {}
 const tagsReducer = (store = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'TAG_CREATE_SUCCESS':
-      return { ...store, tags: [...store.tags, action.response] }
+      return { ...store, tags: [...store.tags.filter(t => t.id !== action.response.id), action.response] }
     case 'TAGS_GET_ALL_SUCCESS':
       return { ...store, tags: action.response }
     default:
