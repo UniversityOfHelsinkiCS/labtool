0d553554b09f720500497dc0075e5454832c6a22
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-06-14 14:36:19 +0300

    add backend support for tagging a student

--- backend/server/routes/tagRoutes.js
+++ backend/server/routes/tagRoutes.js
@@ -3,4 +3,5 @@ const tagController = require('../controllers').tags
 module.exports = app => {
   app.post('/api/tags/create/', tagController.createOrUpdate)
   app.get('/api/tags/list/', tagController.getAll)
+  app.post('/api/tags/tagStudent/', tagController.addTagToStudentInstance)
 }
