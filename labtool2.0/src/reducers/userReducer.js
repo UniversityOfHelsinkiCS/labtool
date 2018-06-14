a1bd894ef567dbea1c78d81fa2afe46a81f45a71
Chamion <jemi.salo@gmail.com>
2018-06-14 17:39:22 +0300

    Sort users and have unnamed users be represented by their username.

--- labtool2.0/src/reducers/userReducer.js
+++ labtool2.0/src/reducers/userReducer.js
@@ -1,10 +1,19 @@
+import { sortUsers } from '../util/sort'
+
 /**
  * Named users in state. Contains all the users in the labtool database.
  */
 const userReducer = (store = [], action) => {
   switch (action.type) {
-    case 'USERS_GET_ALL_SUCCESS':
-      return action.response
+    case 'USERS_GET_ALL_SUCCESS': {
+      const users = sortUsers(action.response)
+      users.forEach(user => {
+        if (user.firsts === null) {
+          user.firsts = `username ${user.username}`
+        }
+      })
+      return users
+    }
     default:
       return store
   }
