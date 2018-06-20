e5356bd8e29ee239ec18807a4072819eb0359bde
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-06-20 13:57:21 +0300

    shows review form even if there is no checklist for that week

--- labtool2.0/src/components/pages/ReviewStudent.js
+++ labtool2.0/src/components/pages/ReviewStudent.js
@@ -96,45 +96,47 @@ export class ReviewStudent extends Component {
           checklistPoints = this.props.selectedInstance.weekMaxPoints
         }
       })
-      return (
-        <div className="ReviewStudent" style={{ textAlignVertical: 'center', textAlign: 'center' }}>
-          <h2> {this.props.selectedInstance.name}</h2>
-          <h3>
-            {' '}
-            {studentData[0].User.firsts} {studentData[0].User.lastname}{' '}
-          </h3>
-          <h3> Viikko {this.props.weekNumber} </h3>
-          <Grid>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <h2>Feedback</h2>
-                <Form onSubmit={this.handleSubmit}>
-                  <Form.Group inline unstackable>
-                    <Form.Field>
-                      <label>Points 0-{this.props.selectedInstance.weekMaxPoints}</label>
-
-                      <Input ref={this.reviewPointsRef} name="points" defaultValue={weekData[0] ? weekData[0].points : ''} type="number" step="0.01" style={{ width: '150px', align: 'center' }} />
-                    </Form.Field>
-                  </Form.Group>
-                  <label> Feedback </label>
-                  <Form.Group inline unstackable style={{ textAlignVertical: 'top' }}>
-                    <div ref={this.reviewTextRef}>
-                      {/*Do not add anything else to this div. If you do, you'll break this.copyChecklistOutput.*/}
-                      <Form.TextArea defaultValue={weekData[0] ? weekData[0].feedback : ''} name="comment" style={{ width: '500px', height: '250px' }} />
-                    </div>
-                  </Form.Group>
+    }
+    return (
+      <div className="ReviewStudent" style={{ textAlignVertical: 'center', textAlign: 'center' }}>
+        <h2> {this.props.selectedInstance.name}</h2>
+        <h3>
+          {' '}
+          {studentData[0].User.firsts} {studentData[0].User.lastname}{' '}
+        </h3>
+        <h3> Viikko {this.props.weekNumber} </h3>
+        <Grid>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <h2>Feedback</h2>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group inline unstackable>
                   <Form.Field>
-                    <Button className="ui center floated green button" type="submit">
-                      Save
-                    </Button>
-                    <Link to={`/labtool/browsereviews/${this.props.selectedInstance.ohid}/${studentData[0].id}`} type="Cancel">
-                      <Button className="ui center floated button" type="cancel">
-                        Cancel
-                      </Button>
-                    </Link>
+                    <label>Points 0-{this.props.selectedInstance.weekMaxPoints}</label>
+
+                    <Input ref={this.reviewPointsRef} name="points" defaultValue={weekData[0] ? weekData[0].points : ''} type="number" step="0.01" style={{ width: '150px', align: 'center' }} />
                   </Form.Field>
-                </Form>
-              </Grid.Column>
+                </Form.Group>
+                <label> Feedback </label>
+                <Form.Group inline unstackable style={{ textAlignVertical: 'top' }}>
+                  <div ref={this.reviewTextRef}>
+                    {/*Do not add anything else to this div. If you do, you'll break this.copyChecklistOutput.*/}
+                    <Form.TextArea defaultValue={weekData[0] ? weekData[0].feedback : ''} name="comment" style={{ width: '500px', height: '250px' }} />
+                  </div>
+                </Form.Group>
+                <Form.Field>
+                  <Button className="ui center floated green button" type="submit">
+                    Save
+                  </Button>
+                  <Link to={`/labtool/browsereviews/${this.props.selectedInstance.ohid}/${studentData[0].id}`} type="Cancel">
+                    <Button className="ui center floated button" type="cancel">
+                      Cancel
+                    </Button>
+                  </Link>
+                </Form.Field>
+              </Form>
+            </Grid.Column>
+            {checkList ? (
               <Grid.Column>
                 <h2>Checklist</h2>
                 {checkList ? (
@@ -166,13 +168,13 @@ export class ReviewStudent extends Component {
                   <p>There is no checklist for this week.</p>
                 )}
               </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      )
-    } else {
-      return <div />
-    }
+            ) : (
+              <div />
+            )}
+          </Grid.Row>
+        </Grid>
+      </div>
+    )
   }
 }
 
