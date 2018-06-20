46deb27be02d336a83d45c485c2cc670f564a693
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-06-20 10:44:20 +0300

    Fix sequelize error when no tag selected

--- backend/server/controllers/tags.js
+++ backend/server/controllers/tags.js
@@ -108,45 +108,128 @@ module.exports = {
   },
 
   addTagToStudentInstance(req, res) {
-    helper.controller_before_auth_check_action(req, res)
-
-    try {
-      TeacherInstance.findOne({
-        where: {
-          userId: req.decoded.id
-        }
-      }).then(found => {
-        if (!found) {
-          res.status(400).send('you have to be a teacher to do this')
-          return
-        }
-        StudentInstance.findOne({
+    if (req.body.tagId) {
+      helper.controller_before_auth_check_action(req, res)
+      try {
+        TeacherInstance.findOne({
           where: {
-            id: req.body.studentId
+            userId: req.decoded.id
           }
-        }).then(student => {
-          if (!student) {
-            res.status(404).send('did not found student with that id')
+        }).then(found => {
+          if (!found) {
+            res.status(400).send('you have to be a teacher to do this')
             return
           }
-          Tag.findOne({
+          StudentInstance.findOne({
             where: {
-              id: req.body.tagId
+              id: req.body.studentId
             }
-          }).then(found => {
-            if (!found) {
-              res.status(404).send('did not find a tag with that id')
+          }).then(student => {
+            if (!student) {
+              res.status(404).send('did not found student with that id')
+              return
             }
-            StudentTag.findOrCreate({
+            Tag.findOne({
               where: {
-                tagId: req.body.tagId,
-                studentInstanceId: req.body.studentId
+                id: req.body.tagId
               }
-            }).then(studentTag => {
-              if (!studentTag) {
-                return res.status(400).send('tagging did not succeed')
+            }).then(found => {
+              if (!found) {
+                res.status(404).send('did not find a tag with that id')
               }
+              StudentTag.findOrCreate({
+                where: {
+                  tagId: req.body.tagId,
+                  studentInstanceId: req.body.studentId
+                }
+              }).then(studentTag => {
+                if (!studentTag) {
+                  return res.status(400).send('tagging did not succeed')
+                }
 
+                StudentInstance.findOne({
+                  where: {
+                    id: req.body.studentId
+                  },
+                  attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                  },
+                  include: [
+                    {
+                      model: Week,
+                      attributes: {
+                        exclude: ['createdAt', 'updatedAt']
+                      },
+                      as: 'weeks',
+                      include: [
+                        {
+                          model: Comment,
+                          attributes: {
+                            exclude: ['createdAt', 'updatedAt']
+                          },
+                          as: 'comments'
+                        }
+                      ]
+                    },
+                    {
+                      model: CodeReview,
+                      attributes: {
+                        exclude: ['createdAt', 'updatedAt']
+                      },
+                      as: 'codeReviews'
+                    },
+                    {
+                      model: User,
+                      attributes: {
+                        exclude: ['createdAt', 'updatedAt']
+                      }
+                    },
+                    {
+                      model: Tag,
+                      attributes: ['id', 'name', 'color']
+                    }
+                  ]
+                }).then(student => {
+                  return res.status(200).send(student)
+                })
+              })
+            })
+          })
+        })
+      } catch (e) {
+        res.status(400).send('ei onnistu')
+      }
+    } else {
+      res.status(400).send('no tag selected')
+    }
+  },
+
+  removeTagFromStudentInstance(req, res) {
+    if (req.body.tagId) {
+      helper.controller_before_auth_check_action(req, res)
+      try {
+        TeacherInstance.findOne({
+          where: {
+            userId: req.decoded.id
+          }
+        }).then(found => {
+          if (!found) {
+            res.status(400).send('you have to be a teacher to do this')
+            return
+          }
+          StudentTag.findOne({
+            where: {
+              studentInstanceId: req.body.studentId,
+              tagId: req.body.tagId
+            }
+          }).then(studentTag => {
+            if (!studentTag) {
+              res.status(404).send('did not find the given student tag')
+            }
+            studentTag.destroy().then(removedTag => {
+              if (!removedTag) {
+                return res.status(400).send('removing student tag failed')
+              }
               StudentInstance.findOne({
                 where: {
                   id: req.body.studentId
