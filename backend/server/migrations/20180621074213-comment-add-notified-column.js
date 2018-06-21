5c3d758778bda0be5afd0aa9ee391f47936a598d
Chamion <jemi.salo@gmail.com>
2018-06-21 11:16:18 +0300

    Add column notified to Weeks.

--- backend/server/migrations/20180621074213-comment-add-notified-column.js
+++ backend/server/migrations/20180621074213-comment-add-notified-column.js
@@ -2,14 +2,19 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Comments', 'notified', {
+    queryInterface.addColumn('Comments', 'notified', {
       type: Sequelize.BOOLEAN,
       allowNull: false,
+      defaultValue: false
+    })
+    queryInterface.addColumn('Weeks', 'notified', {
+      type: Sequelize.BOOLEAN,
       defaultValue: false
     })
   },
 
   down: (queryInterface, Sequelize) => {
     queryInterface.removeColumn('Comments', 'notified')
+    queryInterface.removeColumn('Weeks', 'notified')
   }
 }
