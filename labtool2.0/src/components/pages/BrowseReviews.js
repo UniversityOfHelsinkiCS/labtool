7a7df6eff9dbc77ceeb9410455db328a170f395d
Olli Tanttu <olli.tanttu@helsinki.fi>
2018-06-25 13:54:05 +0300

    git

--- labtool2.0/src/components/pages/BrowseReviews.js
+++ labtool2.0/src/components/pages/BrowseReviews.js
@@ -91,7 +91,7 @@ export class BrowseReviews extends Component {
             const weeks = student.weeks.find(week => week.weekNumber === i + 1)
             if (weeks) {
               headers.push(
-                <Accordion key={i} fluid styled>
+                <Accordion key={weeks.id * Math.floor(Math.random() * 999999999)} fluid styled>
                   <Accordion.Title active={activeIndex === i} index={i} onClick={this.handleClick}>
                     <Icon name="dropdown" /> Week {i + 1}, points {weeks.points}
                   </Accordion.Title>
@@ -121,7 +121,7 @@ export class BrowseReviews extends Component {
                                 </Comment.Content>
                               </Comment>
                             ) : (
-                              <Comment key={comment.id}>
+                              <Comment key={comment.id * Math.floor(Math.random() * 999999999)}>
                                 <Comment.Author>{comment.from}</Comment.Author>
                                 <Comment.Text>
                                   {' '}
@@ -148,7 +148,7 @@ export class BrowseReviews extends Component {
               )
             } else {
               headers.push(
-                <Accordion key={i} fluid styled>
+                <Accordion key={Math.floor(Math.random() * 999999999)} fluid styled>
                   <Accordion.Title active={activeIndex === i} index={i} onClick={this.handleClick}>
                     <Icon name="dropdown" /> Week {i + 1}{' '}
                   </Accordion.Title>
@@ -169,7 +169,8 @@ export class BrowseReviews extends Component {
             })
             .forEach(cr => {
               headers.push(
-                <Accordion key={i + ii} fluid styled>
+                <Accordion key={cr.id * Math.floor(Math.random() * 999999999)} fluid styled>
+                  {' '}
                   <Accordion.Title active={activeIndex === i + ii} index={i + ii} onClick={this.handleClick}>
                     <Icon name="dropdown" /> Code Review {cr.reviewNumber}{' '}
                   </Accordion.Title>
@@ -193,7 +194,7 @@ export class BrowseReviews extends Component {
             const finalWeek = student.weeks.find(week => week.weekNumber === this.props.selectedInstance.weekAmount + 1)
             if (finalWeek) {
               headers.push(
-                <Accordion key={i + ii + 1} fluid styled>
+                <Accordion key={finalWeek.id * Math.floor(Math.random() * 999999999)} fluid styled>
                   <Accordion.Title active={activeIndex === i + ii} index={i + ii} onClick={this.handleClick}>
                     <Icon name="dropdown" /> Final Review, points {finalWeek.points}
                   </Accordion.Title>
@@ -251,7 +252,7 @@ export class BrowseReviews extends Component {
               )
             } else {
               headers.push(
-                <Accordion key={i + ii + 1} fluid styled>
+                <Accordion key={Math.floor(Math.random() * 999999999)} fluid styled>
                   <Accordion.Title active={activeIndex === i} index={i} onClick={this.handleClick}>
                     <Icon name="dropdown" /> Final Review{' '}
                   </Accordion.Title>
