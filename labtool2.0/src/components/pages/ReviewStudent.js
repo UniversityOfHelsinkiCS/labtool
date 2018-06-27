d1bd70e3cf0bcd8939ae76ee10f10e1aadc8a549
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-06-27 12:42:14 +0300

    adds validating props

--- labtool2.0/src/components/pages/ReviewStudent.js
+++ labtool2.0/src/components/pages/ReviewStudent.js
@@ -72,107 +72,111 @@ export class ReviewStudent extends Component {
     if (this.props.loading.redirect) {
       return <Redirect to={`/labtool/courses/${this.props.selectedInstance.ohid}`} />
     }
-    //this.props.ownProps.studentInstance is a string, therefore casting to number.
-    const studentData = this.props.courseData.data.filter(dataArray => dataArray.id === Number(this.props.ownProps.studentInstance))
-    //this.props.weekNumber is a string, therefore casting to number.
-    const weekData = studentData[0].weeks.filter(theWeek => theWeek.weekNumber === Number(this.props.ownProps.weekNumber))
-    const checkList = this.props.selectedInstance.checklists.find(checkl => checkl.week === Number(this.props.ownProps.weekNumber))
-    let checklistOutput = ''
-    let checklistPoints = 0
-    if (checkList) {
-      Object.keys(checkList.list).forEach(cl => {
-        checkList.list[cl].forEach(row => {
-          const addition = this.props.weekReview.checks[row.name] ? row.textWhenOn : row.textWhenOff
-          if (addition) checklistOutput += addition + '\n\n'
-          if (this.props.weekReview.checks[row.name]) {
-            checklistPoints += row.points
+    if (Array.isArray(this.props.courseData.data)) {
+      //this.props.ownProps.studentInstance is a string, therefore casting to number.
+      const studentData = this.props.courseData.data.filter(dataArray => dataArray.id === Number(this.props.ownProps.studentInstance))
+      //this.props.weekNumber is a string, therefore casting to number.
+      const weekData = studentData[0].weeks.filter(theWeek => theWeek.weekNumber === Number(this.props.ownProps.weekNumber))
+      const checkList = this.props.selectedInstance.checklists.find(checkl => checkl.week === Number(this.props.ownProps.weekNumber))
+      let checklistOutput = ''
+      let checklistPoints = 0
+      if (checkList) {
+        Object.keys(checkList.list).forEach(cl => {
+          checkList.list[cl].forEach(row => {
+            const addition = this.props.weekReview.checks[row.name] ? row.textWhenOn : row.textWhenOff
+            if (addition) checklistOutput += addition + '\n\n'
+            if (this.props.weekReview.checks[row.name]) {
+              checklistPoints += row.points
+            }
+          })
+          if (checklistPoints < 0) {
+            checklistPoints = 0
+          } else if (checklistPoints > this.props.selectedInstance.weekMaxPoints) {
+            checklistPoints = this.props.selectedInstance.weekMaxPoints
           }
         })
-        if (checklistPoints < 0) {
-          checklistPoints = 0
-        } else if (checklistPoints > this.props.selectedInstance.weekMaxPoints) {
-          checklistPoints = this.props.selectedInstance.weekMaxPoints
-        }
-      })
-    }
-    return (
-      <div className="ReviewStudent" style={{ textAlignVertical: 'center', textAlign: 'center' }}>
-        <h2> {this.props.selectedInstance.name}</h2>
-        <h3>
-          {' '}
-          {studentData[0].User.firsts} {studentData[0].User.lastname}{' '}
-        </h3>
-        {this.props.weekNumber > this.props.selectedInstance.weekAmount ? <h1>Final Review</h1> : <h3>Viikko {this.props.weekNumber}</h3>}
-        <Grid>
-          <Grid.Row columns={2}>
-            <Grid.Column>
-              <h2>Feedback</h2>
-              <Form onSubmit={this.handleSubmit}>
-                <Form.Group inline unstackable>
-                  <Form.Field>
-                    <label>Points 0-{this.props.selectedInstance.weekMaxPoints}</label>
-
-                    <Input ref={this.reviewPointsRef} name="points" defaultValue={weekData[0] ? weekData[0].points : ''} type="number" step="0.01" style={{ width: '150px', align: 'center' }} />
-                  </Form.Field>
-                </Form.Group>
-                <label> Feedback </label>
-                <Form.Group inline unstackable style={{ textAlignVertical: 'top' }}>
-                  <div ref={this.reviewTextRef}>
-                    {/*Do not add anything else to this div. If you do, you'll break this.copyChecklistOutput.*/}
-                    <Form.TextArea defaultValue={weekData[0] ? weekData[0].feedback : ''} name="comment" style={{ width: '500px', height: '250px' }} />
-                  </div>
-                </Form.Group>
-                <Form.Field>
-                  <Button className="ui center floated green button" type="submit">
-                    Save
-                  </Button>
-                  <Link to={`/labtool/browsereviews/${this.props.selectedInstance.ohid}/${studentData[0].id}`} type="Cancel">
-                    <Button className="ui center floated button" type="cancel">
-                      Cancel
-                    </Button>
-                  </Link>
-                </Form.Field>
-              </Form>
-            </Grid.Column>
-            {checkList ? (
+      }
+      return (
+        <div className="ReviewStudent" style={{ textAlignVertical: 'center', textAlign: 'center' }}>
+          <h2> {this.props.selectedInstance.name}</h2>
+          <h3>
+            {' '}
+            {studentData[0].User.firsts} {studentData[0].User.lastname}{' '}
+          </h3>
+          {this.props.weekNumber > this.props.selectedInstance.weekAmount ? <h1>Final Review</h1> : <h3>Viikko {this.props.weekNumber}</h3>}
+          <Grid>
+            <Grid.Row columns={2}>
               <Grid.Column>
-                <h2>Checklist</h2>
-                {checkList ? (
-                  <div className="checklist">
-                    {Object.keys(checkList.list).map(cl => (
-                      <Card className="checklistCard" fluid color="red" key={cl}>
-                        <Card.Content header={cl} />
-                        {checkList.list[cl].map(row => (
-                          <Card.Content className="checklistCardRow" key={row.name}>
-                            <Form.Field>
-                              <label>{row.name} </label>
-                              <Input type="checkbox" onChange={this.toggleCheckbox(row.name)} />
-                              <label> {row.points} p</label>
-                            </Form.Field>
-                          </Card.Content>
-                        ))}
-                      </Card>
-                    ))}
-                    <div>
-                      <Form className="checklistOutput" onSubmit={this.copyChecklistOutput}>
-                        <Form.TextArea className="checklistOutputText" name="text" value={checklistOutput} style={{ width: '100%', height: '250px' }} />
-                        <p className="checklistOutputPoints">points: {checklistPoints}</p>
-                        <input type="hidden" name="points" value={checklistPoints} />
-                        <Button type="submit">Copy to review fields</Button>
-                      </Form>
+                <h2>Feedback</h2>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group inline unstackable>
+                    <Form.Field>
+                      <label>Points 0-{this.props.selectedInstance.weekMaxPoints}</label>
+
+                      <Input ref={this.reviewPointsRef} name="points" defaultValue={weekData[0] ? weekData[0].points : ''} type="number" step="0.01" style={{ width: '150px', align: 'center' }} />
+                    </Form.Field>
+                  </Form.Group>
+                  <label> Feedback </label>
+                  <Form.Group inline unstackable style={{ textAlignVertical: 'top' }}>
+                    <div ref={this.reviewTextRef}>
+                      {/*Do not add anything else to this div. If you do, you'll break this.copyChecklistOutput.*/}
+                      <Form.TextArea defaultValue={weekData[0] ? weekData[0].feedback : ''} name="comment" style={{ width: '500px', height: '250px' }} />
                     </div>
-                  </div>
-                ) : (
-                  <p>There is no checklist for this week.</p>
-                )}
+                  </Form.Group>
+                  <Form.Field>
+                    <Button className="ui center floated green button" type="submit">
+                      Save
+                    </Button>
+                    <Link to={`/labtool/browsereviews/${this.props.selectedInstance.ohid}/${studentData[0].id}`} type="Cancel">
+                      <Button className="ui center floated button" type="cancel">
+                        Cancel
+                      </Button>
+                    </Link>
+                  </Form.Field>
+                </Form>
               </Grid.Column>
-            ) : (
-              <div />
-            )}
-          </Grid.Row>
-        </Grid>
-      </div>
-    )
+              {checkList ? (
+                <Grid.Column>
+                  <h2>Checklist</h2>
+                  {checkList ? (
+                    <div className="checklist">
+                      {Object.keys(checkList.list).map(cl => (
+                        <Card className="checklistCard" fluid color="red" key={cl}>
+                          <Card.Content header={cl} />
+                          {checkList.list[cl].map(row => (
+                            <Card.Content className="checklistCardRow" key={row.name}>
+                              <Form.Field>
+                                <label>{row.name} </label>
+                                <Input type="checkbox" onChange={this.toggleCheckbox(row.name)} />
+                                <label> {row.points} p</label>
+                              </Form.Field>
+                            </Card.Content>
+                          ))}
+                        </Card>
+                      ))}
+                      <div>
+                        <Form className="checklistOutput" onSubmit={this.copyChecklistOutput}>
+                          <Form.TextArea className="checklistOutputText" name="text" value={checklistOutput} style={{ width: '100%', height: '250px' }} />
+                          <p className="checklistOutputPoints">points: {checklistPoints}</p>
+                          <input type="hidden" name="points" value={checklistPoints} />
+                          <Button type="submit">Copy to review fields</Button>
+                        </Form>
+                      </div>
+                    </div>
+                  ) : (
+                    <p>There is no checklist for this week.</p>
+                  )}
+                </Grid.Column>
+              ) : (
+                <div />
+              )}
+            </Grid.Row>
+          </Grid>
+        </div>
+      )
+    } else {
+      return <Loader active />
+    }
   }
 }
 
