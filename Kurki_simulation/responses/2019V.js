53164469fd0b7a8f0f43e58586f64e50b5b3a96c
Uotila Terho <terho.uotila@helsinki.fi>
2018-08-08 16:43:45 +0300

    more kurki simulation data

---
+++ Kurki_simulation/responses/2019V.js
@@ -0,0 +1,27 @@
+exports.courses = [
+
+  {
+    "id": "TKT20011.20189V.K.1",
+    "name": "Aineopintojen harjoitustyö: Tietokantasovellus (Alkukesä)",
+    "starts": "2018-05-11T21:00:00.000Z",
+    "ends": "2018-06-29T21:00:00.000Z"
+  },
+  {
+    "id": "TKT20010.2019.V.K.1",
+    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit (Alkukesä)",
+    "starts": "2018-05-16T21:00:00.000Z",
+    "ends": "2018-06-11T21:00:00.000Z"
+  },
+  {
+    "id": "TKT20011.2019.V.K.2",
+    "name": "Aineopintojen harjoitustyö: Tietokantasovellus (Loppukesä)",
+    "starts": "2018-07-27T21:00:00.000Z",
+    "ends": "2018-08-29T21:00:00.000Z"
+  },
+  {
+    "id": "TKT20010.2019.V.K.2",
+    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit (Loppukesä)",
+    "starts": "2018-07-27T21:00:00.000Z",
+    "ends": "2018-08-29T21:00:00.000Z"
+  }
+]
