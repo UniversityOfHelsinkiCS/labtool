c23a8ea4f708412ff8937d3e0a7342304aa6024a
Jere Helén <jere.helen@helsinki.fi>
2019-08-13 10:26:35 +0300

    DESTROY service worker.
    Service worker is causing problems with shibboleth login, destroy.

--- labtool2.0/src/index.js
+++ labtool2.0/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import registerServiceWorker from './registerServiceWorker'
 import App from './App'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
@@ -20,5 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-registerServiceWorker()
