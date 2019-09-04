b1577f0361fb5f9fa6ec79624263950e65429526
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-09-04 16:35:41 +0300

    Create LogoutButton component

---
+++ labtool2.0/src/components/LogoutButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { withRouter } from "react-router"
+import { connect } from 'react-redux'
+import { logout } from "../reducers/loginReducer"
+import { Button } from 'semantic-ui-react'
+
+export const LogoutButton = ({ logout, history }) => (
+    <Button onClick={() => {
+        logout()
+        history.push('/')
+    }}>
+        Logout
+    </Button>
+)
+
+export default withRouter(connect(null, { logout })(LogoutButton))
