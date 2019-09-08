0632cd3d6a7aa3d782899fd27ece54525c7cfa0e
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-09-08 16:19:06 +0300

    Create generic component for exporting data as JSON

---
+++ labtool2.0/src/components/JsonExport.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button, Header, Segment, Portal } from "semantic-ui-react";
+
+export default class JsonExport extends React.Component {
+    state = { open: false }
+
+    close = () => this.setState({ open: false })
+    open = () => this.setState({ open: true })
+
+    render() {
+        const { data } = this.props
+        const { open } = this.state
+
+        return (
+            <React.Fragment>
+                <Button
+                    content='Export as JSON'
+                    disabled={open}
+                    positive
+                    onClick={this.open}/>
+
+                <Portal onClose={this.close} open={open}>
+                    <Segment
+                        style={{
+                            left: '40%',
+                            top: '30%',
+                            width: '20%',
+                            position: 'fixed',
+                            zIndex: 100
+                        }}>
+                        <Header>JSON</Header>
+                        <pre>
+                            <code>
+                                { JSON.stringify(data, null, 4) }
+                            </code>
+                        </pre>
+                        <Button
+                        content="Close"
+                        negative
+                        onClick={this.close} />
+                    </Segment>
+                </Portal>
+            </React.Fragment>
+        )
+    }
+}
