425f1f792256ede0716c46c400760676e11832e2
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-09-10 11:44:42 +0300

    Lint

--- labtool2.0/src/components/JsonExport.js
+++ labtool2.0/src/components/JsonExport.js
@@ -1,47 +1,40 @@
-import React from "react";
-import { Button, Header, Segment, Portal } from "semantic-ui-react";
+import React from 'react'
+import { Button, Header, Segment, Portal } from 'semantic-ui-react'
 
 export default class JsonExport extends React.Component {
-    state = { open: false }
+  state = { open: false }
 
-    close = () => this.setState({ open: false })
-    open = () => this.setState({ open: true })
+  close = () => this.setState({ open: false })
+  open = () => this.setState({ open: true })
 
-    render() {
-        const { data } = this.props
-        const { open } = this.state
+  render() {
+    const { data } = this.props
+    const { open } = this.state
 
-        return (
-            <React.Fragment>
-                <Button
-                    disabled={open}
-                    positive
-                    onClick={this.open}>
-                    Export as JSON
-                </Button>
+    return (
+      <React.Fragment>
+        <Button disabled={open} positive onClick={this.open}>
+          Export as JSON
+        </Button>
 
-                <Portal onClose={this.close} open={open}>
-                    <Segment
-                        style={{
-                            left: '40%',
-                            top: '30%',
-                            width: '20%',
-                            position: 'fixed',
-                            zIndex: 100
-                        }}>
-                        <Header>JSON</Header>
-                        <pre>
-                            <code>
-                                { JSON.stringify(data, null, 4) }
-                            </code>
-                        </pre>
-                        <Button
-                        content="Close"
-                        negative
-                        onClick={this.close} />
-                    </Segment>
-                </Portal>
-            </React.Fragment>
-        )
-    }
+        <Portal onClose={this.close} open={open}>
+          <Segment
+            style={{
+              left: '40%',
+              top: '30%',
+              width: '20%',
+              position: 'fixed',
+              zIndex: 100
+            }}
+          >
+            <Header>JSON</Header>
+            <pre>
+              <code>{JSON.stringify(data, null, 4)}</code>
+            </pre>
+            <Button content="Close" negative onClick={this.close} />
+          </Segment>
+        </Portal>
+      </React.Fragment>
+    )
+  }
 }
--- labtool2.0/src/tests/JsonExport.test.js
+++ labtool2.0/src/tests/JsonExport.test.js
@@ -1,32 +1,41 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { Button } from "semantic-ui-react";
-import JsonExport from "../components/JsonExport";
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Button } from 'semantic-ui-react'
+import JsonExport from '../components/JsonExport'
 
-describe("<JsonExport />", () => {
-    const data = { a: 'a', b: [1, 2, 3] }
-    const dataString = JSON.stringify(data, null, 4)
+describe('<JsonExport />', () => {
+  const data = { a: 'a', b: [1, 2, 3] }
+  const dataString = JSON.stringify(data, null, 4)
 
-    let wrapper
+  let wrapper
 
-    beforeEach(() => {
-        wrapper = shallow(<JsonExport data={data} />)
+  beforeEach(() => {
+    wrapper = shallow(<JsonExport data={data} />)
+  })
+
+  describe('JsonExport component', () => {
+    it('renders ok', () => {
+      expect(wrapper).toBeDefined()
+      expect(
+        wrapper
+          .find(Button)
+          .first()
+          .children()
+          .text()
+      ).toEqual('Export as JSON')
     })
-    
-    describe("JsonExport component", () => {
-        it("renders ok", () => {
-            expect(wrapper).toBeDefined()
-            expect(wrapper.find(Button).first().children().text()).toEqual("Export as JSON")
-        })
 
-        it("displays JSON correctly", () => {
-            expect(wrapper.find("pre code").text()).toEqual(dataString)
-        })
-
-        it("can be clicked to open", () => {
-            wrapper.find(Button).first().simulate('click')
-
-            expect(wrapper.state('open')).toEqual(true)
-        })
+    it('displays JSON correctly', () => {
+      expect(wrapper.find('pre code').text()).toEqual(dataString)
     })
+
+    it('can be clicked to open', () => {
+      wrapper
+        .find(Button)
+        .first()
+        .simulate('click')
+
+      expect(wrapper.state('open')).toEqual(true)
+    })
+  })
 })
