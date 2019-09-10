5694756caf288b8b4f6bcc111a907cf053eac5b0
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-09-10 19:50:08 +0300

    Use modal in JsonExport + allo styling button

--- labtool2.0/src/components/JsonExport.js
+++ labtool2.0/src/components/JsonExport.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Header, Segment, Portal } from 'semantic-ui-react'
+import { Button, Modal } from 'semantic-ui-react'
 
 export default class JsonExport extends React.Component {
   state = { open: false }
@@ -8,32 +8,26 @@ export default class JsonExport extends React.Component {
   open = () => this.setState({ open: true })
 
   render() {
-    const { data } = this.props
+    const { data, style } = this.props
     const { open } = this.state
 
     return (
       <React.Fragment>
-        <Button disabled={open} positive onClick={this.open}>
+        <Button disabled={open} onClick={this.open} style={style}>
           Export as JSON
         </Button>
 
-        <Portal onClose={this.close} open={open}>
-          <Segment
-            style={{
-              left: '40%',
-              top: '30%',
-              width: '20%',
-              position: 'fixed',
-              zIndex: 100
-            }}
-          >
-            <Header>JSON</Header>
-            <pre>
+        <Modal onClose={this.close} open={open}>
+          <Modal.Header>JSON</Modal.Header>
+          <Modal.Description>
+            <pre style={{ margin: 15 }}>
               <code>{JSON.stringify(data, null, 4)}</code>
             </pre>
+          </Modal.Description>
+          <Modal.Actions>
             <Button content="Close" negative onClick={this.close} />
-          </Segment>
-        </Portal>
+          </Modal.Actions>
+        </Modal>
       </React.Fragment>
     )
   }
