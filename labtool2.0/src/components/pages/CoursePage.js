ff917097ee572667d72a5b9f732c805a9c6728d6
hisahi <sampo.hippelainen@gmail.com>
2019-09-11 16:43:41 +0300

    fix column bug on course view

--- labtool2.0/src/components/pages/CoursePage.js
+++ labtool2.0/src/components/pages/CoursePage.js
@@ -662,6 +662,12 @@ export class CoursePage extends React.Component {
                         {data.User.firsts} {data.User.lastname}
                       </Table.Cell>
                       <Table.Cell>
+                        <span>{data.User.studentNumber}</span>
+                      </Table.Cell>
+                      <Table.Cell>
+                        <a href={`mailto:${data.User.email}`}>{data.User.email}</a>
+                      </Table.Cell>
+                      <Table.Cell>
                         <span>
                           {data.projectName}
                           <br />
@@ -759,6 +765,8 @@ export class CoursePage extends React.Component {
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell key={-1}>Student</Table.HeaderCell>
+                <Table.HeaderCell>id</Table.HeaderCell>
+                <Table.HeaderCell>email</Table.HeaderCell>
                 <Table.HeaderCell>Project Info</Table.HeaderCell>
                 {createHeadersTeacher()}
                 <Table.HeaderCell> Sum </Table.HeaderCell>
