fff1dafe4a9067228f9f9ae69d71713255b15f8b
Salmi Sara M <salmisar@cs.helsinki.fi>
2019-09-28 15:07:18 +0300

    Corrected a typo

--- labtool2.0/src/components/pages/CoursePage.js
+++ labtool2.0/src/components/pages/CoursePage.js
@@ -160,7 +160,7 @@ export class CoursePage extends React.Component {
     return this.props.tags.tags && this.props.tags.tags.map(tag => tag.name.toUpperCase()).includes('DROPPED')
   }
 
-  droppedStudentExist = () => {
+  droppedStudentExists = () => {
     let dropped = this.props.courseData.data.map(student => student.dropped)
     return dropped.includes(true)
   }
@@ -824,7 +824,7 @@ export class CoursePage extends React.Component {
     }
 
     let renderTeacherBottomPartForDroppedOutStudents = () => {
-      if (this.droppedStudentExist()) {
+      if (this.droppedStudentExists()) {
         return renderTeacherBottomPartForStudents(true)
       }
     }
