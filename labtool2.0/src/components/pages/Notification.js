32b1fec35807176dbe6c1394a8b0c250ceb40d70
luoyumo <yumo.luo@hotmail.com>
2019-10-06 23:14:14 +0300

    use modal when showing a notification

--- labtool2.0/src/components/pages/Notification.js
+++ labtool2.0/src/components/pages/Notification.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { clearNotifications } from '../../reducers/notificationReducer'
-import { Message } from 'semantic-ui-react'
+import { Message, Modal, Button, Icon } from 'semantic-ui-react'
 
 let timeout
 
@@ -10,34 +10,91 @@ let timeout
  *  Notification, that clears itself after a set of time.
  */
 export const Notification = props => {
+  const [open, setOpen] = useState(false)
+
   useEffect(() => {
-    const { message } = props.notification
-    if (message !== undefined) {
-      clearTimeout(timeout)
-      timeout = setTimeout(() => {
-        props.clearNotifications()
-      }, 5000)
-    }
+    setOpen(true)
+    // const { message } = props.notification
+    // if (message !== undefined) {
+    //   clearTimeout(timeout)
+    //   timeout = setTimeout(() => {
+    //     props.clearNotifications()
+    //   }, 5000)
+    // }
   }, [props.notification.message])
+
+  // const handleOpen = () => {
+  //   if (message) {
+  //     setOpen(true)
+  //   }
+  // }
+
+  const handleClose = () => {
+    props.clearNotifications()
+    setOpen(false)
+  }
 
   const message = props.notification.message
   const error = props.notification.error
 
+  const createNotification = () => (
+    <Modal open={open} basic size="small">
+      <Modal.Content>
+        <Message className={error ? 'error' : 'success'} color={error ? 'red' : 'green'} size="large">
+          {message}
+        </Message>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="grey" onClick={handleClose} inverted>
+          <Icon name="checkmark" /> Got it
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  )
+
   if (message === undefined) {
     return <div />
-  } else if (error) {
-    return (
-      <Message className="error" color="red" size="large">
-        {message}
-      </Message>
-    )
   } else {
-    return (
-      <Message className="success" color="green" size="large">
-        {message}
-      </Message>
-    )
+    return <div>{createNotification()}</div>
+    // <Modal open={open} basic size="small">
+    //   {console.log('open', open)}
+    //   {console.log('message', props.notification.message)}
+    //   <Modal.Content>
+    //     <Message className="error" color="red" size="large">
+    //       {message}
+    //     </Message>
+    //   </Modal.Content>
+    //   <Modal.Actions>
+    //     <Button color="grey" onClick={handleClose} inverted>
+    //       <Icon name="checkmark" /> Got it
+    //     </Button>
+    //   </Modal.Actions>
+    // </Modal>
+    // // <Message className="error" color="red" size="large">
+    // //   {message}
+    // // </Message>
   }
+  // else {
+  //   return (
+  //     <Modal open={open} basic size="small">
+  //       {console.log('open', open)}
+  //       {console.log('message', props.notification.message)}
+  //       <Modal.Content>
+  //         <Message className="success" color="green" size="large">
+  //           {message}
+  //         </Message>
+  //       </Modal.Content>
+  //       <Modal.Actions>
+  //         <Button color="grey" onClick={handleClose} inverted>
+  //           <Icon name="checkmark" /> Got it
+  //         </Button>
+  //       </Modal.Actions>
+  //     </Modal>
+  //     // <Message className="success" color="green" size="large">
+  //     //   {message}
+  //     // </Message>
+  //   )
+  // }
 }
 
 const mapStateToProps = state => {
