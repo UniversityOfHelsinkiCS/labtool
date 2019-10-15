c59e2cd8737dc877746099626cc2326c5218ad13
Salmi Sara M <salmisar@cs.helsinki.fi>
2019-10-15 18:04:33 +0300

    Add info popup next to the maximum points field and a tiny fix

--- labtool2.0/src/components/pages/ReviewStudent.js
+++ labtool2.0/src/components/pages/ReviewStudent.js
@@ -104,7 +104,7 @@ export const ReviewStudent = props => {
 
   const getMaximumPoints = () => {
     const checklist = props.selectedInstance.checklists.find(checkl => checkl.week === Number(props.ownProps.weekNumber))
-    if (checklist == undefined || !checklist.maxPoints) {
+    if (checklist === undefined || !checklist.maxPoints) {
       return props.selectedInstance.weekMaxPoints
     }
     return checklist.maxPoints
