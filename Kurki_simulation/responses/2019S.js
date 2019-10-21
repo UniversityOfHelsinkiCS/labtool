735a734d6a11657dcb7cdbd681dc651e9c9f5c8c
luoyumo <yumo.luo@hotmail.com>
2019-10-21 21:50:07 +0300

    change start and end dates of importable courses

--- Kurki_simulation/responses/2019S.js
+++ Kurki_simulation/responses/2019S.js
@@ -1,32 +1,32 @@
 exports.courses = [
   {
     "id": "TKT20010.2019.S.K.1",
-    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit",
-    "starts": "2018-09-16T21:00:00.000Z",
-    "ends": "2018-10-11T21:00:00.000Z"
+    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit (periodi I)",
+    "starts": "2019-09-16T21:00:00.000Z",
+    "ends": "2019-10-11T21:00:00.000Z"
   },
   {
     "id": "TKT20002.2019.S.K.1",
     "name": "Ohjelmistotekniikan menetelmät",
-    "starts": "2018-09-11T21:00:00.000Z",
-    "ends": "2018-10-29T21:00:00.000Z"
+    "starts": "2019-09-11T21:00:00.000Z",
+    "ends": "2019-10-29T21:00:00.000Z"
   },
   {
     "id": "TKT20011.2019.S.K.1",
-    "name": "Aineopintojen harjoitustyö: Tietokantasovellus",
-    "starts": "2018-09-11T21:00:00.000Z",
-    "ends": "2018-10-29T21:00:00.000Z"
+    "name": "Aineopintojen harjoitustyö: Tietokantasovellus (periodi I)",
+    "starts": "2019-09-11T21:00:00.000Z",
+    "ends": "2019-10-29T21:00:00.000Z"
   },
   {
     "id": "TKT20010.2019.S.K.2",
-    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit",
-    "starts": "2018-10-16T21:00:00.000Z",
-    "ends": "2018-12-11T21:00:00.000Z"
+    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit (periodi II)",
+    "starts": "2019-10-16T21:00:00.000Z",
+    "ends": "2019-12-11T21:00:00.000Z"
   },
   {
     "id": "TKT20011.2019.S.K.2",
-    "name": "Aineopintojen harjoitustyö: Tietokantasovellus",
-    "starts": "2018-10-11T21:00:00.000Z",
-    "ends": "2018-12-29T21:00:00.000Z"
+    "name": "Aineopintojen harjoitustyö: Tietokantasovellus (periodi II)",
+    "starts": "2019-10-11T21:00:00.000Z",
+    "ends": "2019-12-29T21:00:00.000Z"
   }
 ]
--- Kurki_simulation/responses/2020K.js
+++ Kurki_simulation/responses/2020K.js
@@ -2,32 +2,32 @@ exports.courses = [
 
   {
     "id": "TKT20010.2020.K.A.1",
-    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit",
-    "starts": "2018-01-16T21:00:00.000Z",
-    "ends": "2018-03-11T21:00:00.000Z"
+    "name": "Aineopintojen harjoitustyö: Tietorakenteet ja algoritmit (periodi III)",
+    "starts": "2020-01-16T21:00:00.000Z",
+    "ends": "2020-03-11T21:00:00.000Z"
   },
   {
     "id": "TKT20002.2020.K.K.1",
     "name": "Ohjelmistotekniikan menetelmät",
-    "starts": "2018-03-11T21:00:00.000Z",
-    "ends": "2018-04-29T21:00:00.000Z"
+    "starts": "2020-03-11T21:00:00.000Z",
+    "ends": "2020-04-29T21:00:00.000Z"
   },
   {
     "id": "TKT20011.2020.K.K.1",
-    "name": "Aineopintojen harjoitustyö: Tietokantasovellus",
-    "starts": "2018-03-11T21:00:00.000Z",
-    "ends": "2018-04-29T21:00:00.000Z"
+    "name": "Aineopintojen harjoitustyö: Tietokantasovellus (periodi IV)",
+    "starts": "2020-03-11T21:00:00.000Z",
+    "ends": "2020-04-29T21:00:00.000Z"
   },
   {
     "id": "TKT20012.2020.K.K.1",
-    "name": "Aineopintojen harjoitustyö: Tietoliikenne",
-    "starts": "2018-01-16T21:00:00.000Z",
-    "ends": "2018-03-11T21:00:00.000Z"
+    "name": "Aineopintojen harjoitustyö: Tietoliikenne (periodi III)",
+    "starts": "2020-01-16T21:00:00.000Z",
+    "ends": "2020-03-11T21:00:00.000Z"
   },
   {
     "id": "TKT20666.2020.K.A.1",
-    "name": "Aineopintojen harjoituskurssi: Opettajalla ei opiskelijanumeroa",
-    "starts": "2018-01-16T21:00:00.000Z",
-    "ends": "2018-03-11T21:00:00.000Z"
+    "name": "Aineopintojen harjoituskurssi: Opettajalla ei opiskelijanumeroa (periodi III)",
+    "starts": "2020-01-16T21:00:00.000Z",
+    "ends": "2020-03-11T21:00:00.000Z"
   }
 ]
