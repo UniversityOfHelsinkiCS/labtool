f12ede74267f04bbfaf9cc73c2f0144664932b47
hisahi <sampo.hippelainen@gmail.com>
2019-11-10 18:35:37 +0200

    make purpose of 'invalid registration' clearer; student messages for dropped/invalid

--- labtool2.0/src/tests/BrowseReviews.test.js
+++ labtool2.0/src/tests/BrowseReviews.test.js
@@ -309,7 +309,8 @@ describe('<BrowseReviews />', () => {
       })
       it('students course registration can be marked as valid and invalid', () => {
         wrapper = wrapper.find('StudentCard').dive()
-        wrapper.find({ children: 'Mark registration as valid' }).simulate('click')
+        //wrapper.find({ children: 'Mark registration as valid' }).simulate('click')
+        shallow(wrapper.find('Popup').prop('trigger')).dive().find('#buttonInvalidRegistration').simulate('click')
 
         expect(mockUpdateStudentProjectInfo).toBeCalledWith(expect.objectContaining({ validRegistration: true }))
       })
