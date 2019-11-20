d91877105368cf945c27deb994e42c5572644b8d
hisahi <sampo.hippelainen@gmail.com>
2019-11-20 19:20:43 +0200

    update snaps; no connect on Points

--- labtool2.0/src/components/Points.js
+++ labtool2.0/src/components/Points.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import { roundPoints } from '../util/format'
 
 export const Points = ({ points }) => (points !== null ? roundPoints(points) : '-')
@@ -8,7 +7,4 @@ Points.propTypes = {
   points: PropTypes.number
 }
 
-export default connect(
-  null,
-  {}
-)(Points)
+export default Points
