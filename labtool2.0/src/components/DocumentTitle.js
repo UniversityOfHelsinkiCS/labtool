5c14533adaaa22647519d99ba2eee46d352acb81
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-11-20 19:41:23 +0200

    Remove 2.0 from document title

--- labtool2.0/src/components/DocumentTitle.js
+++ labtool2.0/src/components/DocumentTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 export default ({ title }) => (
-  <Helmet titleTemplate={'%s | Labtool 2.0'} defaultTitle={'Labtool 2.0'}>
+  <Helmet titleTemplate={'%s | Labtool'} defaultTitle={'Labtool'}>
     <title>{title}</title>
   </Helmet>
 )
