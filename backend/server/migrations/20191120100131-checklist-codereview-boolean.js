3b03a78c9636e1b79a3ac0cbc2a34eb857521370
Sampo Hippeläinen <sampo.hippelainen@gmail.com>
2019-11-20 12:12:03 +0200

    codeReviewNumber -> forCodeReview

---
+++ backend/server/migrations/20191120100131-checklist-codereview-boolean.js
@@ -0,0 +1,19 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Checklists', 'forCodeReview', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
+    await queryInterface.sequelize.query('UPDATE "Checklists" SET "forCodeReview" = TRUE WHERE "codeReviewNumber" IS NOT NULL')
+    await queryInterface.removeColumn('Checklists', 'codeReviewNumber')
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Checklists', 'codeReviewNumber', {
+      type: Sequelize.INTEGER,
+      defaultValue: null
+    })
+    await queryInterface.sequelize.query('UPDATE "Checklists" SET "codeReviewNumber" = 1 WHERE "forCodeReview" = TRUE')
+    await queryInterface.removeColumn('Checklists', 'forCodeReview')
+  }
+};
