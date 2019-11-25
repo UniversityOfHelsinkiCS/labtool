b26f197c76de1fb3193fe8ec37fbd14e31e7bb33
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-11-25 14:39:54 +0200

    Update Error componenet

--- labtool2.0/src/components/Error.js
+++ labtool2.0/src/components/Error.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { Message, List } from 'semantic-ui-react'
 
 export default ({ header, errors }) => (
-  <Message error icon="warning sign" header={header || 'Error'}>
-    <Message.Content>
+  <Message
+    error
+    icon="warning sign"
+    header={header || 'Error'}
+    content={
       <List bulleted>
         {errors.map(error => (
           <List.Item key={error}>{error}</List.Item>
         ))}
       </List>
-    </Message.Content>
-  </Message>
+    }
+  />
 )
