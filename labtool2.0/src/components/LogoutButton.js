c7b7c1bf20138e3e7e6acc59dcc0e8b51d5e8865
gotonode <5214152+gotonode@users.noreply.github.com>
2019-11-30 01:21:19 +0200

    Logout-button changes (icon, color)

--- labtool2.0/src/components/LogoutButton.js
+++ labtool2.0/src/components/LogoutButton.js
@@ -2,9 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 
-export const LogoutButton = ({ logout }) => <Button onClick={logout}>Logout</Button>
+export const LogoutButton = ({ logout }) => (
+  <Button style={{ backgroundColor: '#faffd9' }} onClick={logout}>
+    <Icon name="sign out alternate" />
+    Logout
+  </Button>
+)
 
 LogoutButton.propTypes = {
   logout: PropTypes.func.isRequired
