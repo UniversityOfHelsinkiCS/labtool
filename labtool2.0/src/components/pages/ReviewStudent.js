1f560d3bb84fde386dab077394242e06f7ac15bf
Sampo Hippeläinen <sampo.hippelainen@gmail.com>
2019-12-04 13:10:24 +0200

    Array.flat polyfill

--- labtool2.0/src/components/pages/ReviewStudent.js
+++ labtool2.0/src/components/pages/ReviewStudent.js
@@ -21,7 +21,9 @@ import MissingMinimumRequirements from '../MissingMinimumRequirements'
 import BackButton from '../BackButton'
 import DocumentTitle from '../DocumentTitle'
 import Error from '../Error'
+import { Header, Segment, List } from 'semantic-ui-react'
 import { Points } from '../Points'
+import '../../util/arrayFlatPolyfill'
 
 const isFinalReview = props => props.weekNumber > props.selectedInstance.weekAmount
 
