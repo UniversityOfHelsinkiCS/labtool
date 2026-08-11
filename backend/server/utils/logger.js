04bfd2bc7461f18f9907bc727f08be1450feea16
matiassinisalo <matias.aleksanteri@hotmail.fi>
2026-08-11 14:54:28 +0300

    logger update to use new loki backend

--- backend/server/utils/logger.js
+++ backend/server/utils/logger.js
@@ -1,28 +1,64 @@
-const Log2gelf = require('winston-log2gelf')
 const winston = require('winston')
-const moment = require('moment')
+const LokiTransport = require('winston-loki')
 
-const { combine, timestamp, prettyPrint } = winston.format
+const inProduction = process.env.NODE_ENV === 'production'
+
+const LOKI_HOST = process.env.LOKI_HOST || 'http://loki-svc.toska-lokki.svc.cluster.local:3100'
+
 const transports = []
+
 if (process.env.NODE_ENV !== 'test') {
   transports.push(new winston.transports.File({ filename: 'debug.log' }))
 }
-transports.push(new (winston.transports.Console)({ timestamp: true }))
-if (process.env.LOG_PORT && process.env.LOG_HOST) {
-  transports.push(new Log2gelf({
-    hostname: process.env.LOG_HOSTNAME || 'labtool-backend',
-    host: process.env.LOG_HOST,
-    port: process.env.LOG_PORT,
-    protocol: 'http'
+
+if (!inProduction) {
+  // for now we decided to not get staging spam to our logging
+  // const { combine, timestamp, printf, splat } = winston.format
+  //
+  // const devFormat = printf(({ level, message, timestamp, ...rest }) => {
+  //   let restString = ''
+  //   try {
+  //     restString = JSON.stringify(rest)
+  //   } catch (e) {
+  //     restString = 'Error stringifying rest'
+  //   }
+  //
+  //   return `${timestamp} ${level}: ${message} ${restString}`
+  // })
+  //
+  // transports.push(
+  //   new winston.transports.Console({
+  //     level: 'debug',
+  //     format: combine(splat(), timestamp(), devFormat)
+  //   })
+  // )
+} else {
+  const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    verbose: 4,
+    debug: 5,
+    silly: 6
+  }
+
+  const prodFormat = winston.format.printf(({ level, ...rest }) => JSON.stringify({
+    level: levels[level],
+    ...rest
+  }))
+
+  transports.push(new winston.transports.Console({ format: prodFormat }))
+
+  transports.push(new LokiTransport({
+    host: LOKI_HOST,
+    labels: {
+      app: 'labtool',
+      environment: process.env.NODE_ENV || 'production'
+    }
   }))
 }
-const logger = winston.createLogger({
-  format: combine(
-    timestamp({
-      format: () => moment().format('DD-MM-YYYY HH:mm:ss')
-    }),
-    prettyPrint({ depth: 5 })
-  ),
-  transports
-})
+
+const logger = winston.createLogger({ transports })
+
 module.exports = logger
