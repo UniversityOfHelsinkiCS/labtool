dfcb8dcf8470f4bf35e8a00e995f3b4ed0f4414a
Tony Lam <tony.lam@iki.fi>
2026-09-09 12:49:46 +0300

    feat: add ping endpoint with logging

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,6 +1,17 @@
+const logger = require('../utils/logger')
+
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the COURSES API!'
   })
   )
+
+  app.get('/api/ping', (req, res) => {
+    logger.info('Ping endpoint called', {
+    })
+
+    return res.status(200).send({
+      message: 'pong'
+    })
+  })
 }
