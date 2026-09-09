e6db718d589a1436133ef4710b03d42dd3f51d16
Tony Lam <tony.lam@iki.fi>
2026-09-09 13:12:21 +0300

    refactor: update logger to handle both production and staging environments

--- backend/server/utils/logger.js
+++ backend/server/utils/logger.js
@@ -1,7 +1,7 @@
 const winston = require('winston')
 const LokiTransport = require('winston-loki')
 
-const inProduction = process.env.NODE_ENV === 'production'
+const isDeployedEnvironment = ['production', 'staging'].includes(process.env.NODE_ENV)
 
 const LOKI_HOST = process.env.LOKI_HOST || 'http://loki-svc.toska-lokki.svc.cluster.local:3100'
 
@@ -11,28 +11,7 @@ if (process.env.NODE_ENV !== 'test') {
   transports.push(new winston.transports.File({ filename: 'debug.log' }))
 }
 
-if (!inProduction) {
-  // for now we decided to not get staging spam to our logging
-  // const { combine, timestamp, printf, splat } = winston.format
-  //
-  // const devFormat = printf(({ level, message, timestamp, ...rest }) => {
-  //   let restString = ''
-  //   try {
-  //     restString = JSON.stringify(rest)
-  //   } catch (e) {
-  //     restString = 'Error stringifying rest'
-  //   }
-  //
-  //   return `${timestamp} ${level}: ${message} ${restString}`
-  // })
-  //
-  // transports.push(
-  //   new winston.transports.Console({
-  //     level: 'debug',
-  //     format: combine(splat(), timestamp(), devFormat)
-  //   })
-  // )
-} else {
+if (isDeployedEnvironment) {
   const levels = {
     error: 0,
     warn: 1,
@@ -58,7 +37,6 @@ if (!inProduction) {
       environment: process.env.NODE_ENV || 'production'
     }
   }))
-
 }
 
 const logger = winston.createLogger({ transports })
